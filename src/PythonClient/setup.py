3d2bd42dad936ae21554844ea55f647b4b7956e5
Peter Nagy <peter@quix.ai>
2023-02-14 03:06:59 +0000

    Revert to confluent 1.9.2 for now

--- src/PythonClient/setup.py
+++ src/PythonClient/setup.py
@@ -5,7 +5,7 @@ import os.path
 import re
 import fileinput
 
-package_version = "0.5.0.dev15"
+package_version = "0.5.0.dev16"
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
