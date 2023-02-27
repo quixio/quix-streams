2463a7d88d48d61449043c00562b4ba24660517f
Peter Nagy <peter@quix.ai>
2023-02-27 02:36:28 +0000

    bump dev24

--- src/PythonClient/setup.py
+++ src/PythonClient/setup.py
@@ -5,7 +5,7 @@ import os.path
 import re
 import fileinput
 
-package_version = "0.5.0.dev23"
+package_version = "0.5.0.dev24"
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
