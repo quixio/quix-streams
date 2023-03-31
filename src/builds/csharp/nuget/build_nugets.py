10c3ad6089905b365892c639dd64f880540a2aaa
PeterN <79838809+peter-quix@users.noreply.github.com>
2023-03-31 10:45:30 +0100

    Up to 0.5.1.0 release version (#85)

--- src/builds/csharp/nuget/build_nugets.py
+++ src/builds/csharp/nuget/build_nugets.py
@@ -7,8 +7,8 @@ import fileinput
 from typing import List
 
 version = "0.5.1.0"
-informal_version = "0.5.1.0-dev13"
-nuget_version = "0.5.1.0-dev13"
+informal_version = "0.5.1.0"
+nuget_version = "0.5.1.0"
 
 
 def updatecsproj(projfilepath):
