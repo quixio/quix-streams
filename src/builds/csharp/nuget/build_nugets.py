942d0541b66d2eb001762f5563af784b0ef5dde0
Peter Nagy <peter@quix.io>
2023-04-12 13:11:21 +0100

    Update dev package version

--- src/builds/csharp/nuget/build_nugets.py
+++ src/builds/csharp/nuget/build_nugets.py
@@ -7,8 +7,8 @@ import fileinput
 from typing import List
 
 version = "0.5.2.0"
-informal_version = "0.5.2.0-dev1"
-nuget_version = "0.5.2.0-dev1"
+informal_version = "0.5.2.0-dev6"
+nuget_version = "0.5.2.0-dev6"
 
 
 def updatecsproj(projfilepath):
