54a10211bb548e0f3560eb524ff9ddce794c2fda
Peter Nagy <peter@quix.io>
2023-04-18 17:31:21 +0200

    Up version to 0.5.3 and dev 1

--- src/PythonClient/setup.py
+++ src/PythonClient/setup.py
@@ -5,7 +5,7 @@ import os.path
 import re
 import fileinput
 
-package_version = "0.5.3"
+package_version = "0.5.3.dev1"
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
--- src/builds/csharp/nuget/build_nugets.py
+++ src/builds/csharp/nuget/build_nugets.py
@@ -7,8 +7,8 @@ import fileinput
 from typing import List
 
 version = "0.5.3.0"
-informal_version = "0.5.3.0"
-nuget_version = "0.5.3.0"
+informal_version = "0.5.3.0-dev1"
+nuget_version = "0.5.3.0-dev1"
 
 
 def updatecsproj(projfilepath):
