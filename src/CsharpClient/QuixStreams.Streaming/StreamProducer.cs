quixio/quix-streams

commit ef777d40a54ac38526c4be41061f432839a08be1

Add Flush method to the Stream (#112)
Added a flush method to the stream class that flushes stream's Timeseries, Events and Properties data.

This Flush is different from other as it awaits for all messages to be sent before continuing in the same way as stream.Close() awaits

src/CsharpClient/QuixStreams.Streaming/StreamProducer.cs
@@ -221,6 +221,34 @@ namespace QuixStreams.Streaming
             return lastSendTask = base.Send(model);
         }
 
+        public void Flush()
+        {
+            this.Properties.Flush();
+            this.Timeseries.Flush();
+            this.Events.Flush();
+            try
+            {
+                if (lastSendTask != null && !lastSendTask.IsCanceled && !lastSendTask.IsCompleted && !lastSendTask.IsFaulted)
+                {
+                    this.logger.LogTrace("Waiting for last message send for stream {1}.",  this.StreamId);
+                    var sw = Stopwatch.StartNew();
+                    Task.WaitAny(new[] {lastSendTask}, TimeSpan.FromSeconds(10));
+                    sw.Stop();
+                    if (!lastSendTask.IsCanceled && !lastSendTask.IsCompleted && !lastSendTask.IsFaulted)
+                    {
+                        this.logger.LogWarning("Last send did not finish in {0:g} for stream {1}. In future this timeout will be configurable.", sw.Elapsed, this.StreamId);
+                    }
+                    else
+                    {
+                        this.logger.LogTrace("Finished waiting for last message send in {0:g} for stream {1}.", sw.Elapsed, this.StreamId);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                this.logger.LogWarning(ex, "Last send did not finish successfully for stream {1}.", this.StreamId);
+            }
+        }        
 
         /// <inheritdoc />
         public void Close(QuixStreams.Telemetry.Models.StreamEndType streamState = QuixStreams.Telemetry.Models.StreamEndType.Closed)
@@ -250,29 +278,8 @@ namespace QuixStreams.Streaming
 
                 // Close stream
                 base.Close();
-
-                try
-                {
-                    if (lastSendTask != null && !lastSendTask.IsCanceled && !lastSendTask.IsCompleted && !lastSendTask.IsFaulted)
-                    {
-                        this.logger.LogTrace("Waiting for last message send for stream {1}.",  this.StreamId);
-                        var sw = Stopwatch.StartNew();
-                        Task.WaitAny(new[] {lastSendTask}, TimeSpan.FromSeconds(10));
-                        sw.Stop();
-                        if (!lastSendTask.IsCanceled && !lastSendTask.IsCompleted && !lastSendTask.IsFaulted)
-                        {
-                            this.logger.LogWarning("Last send did not finish in {0:g} for stream {1}. In future this timeout will be configurable.", sw.Elapsed, this.StreamId);
-                        }
-                        else
-                        {
-                            this.logger.LogTrace("Finished waiting for last message send in {0:g} for stream {1}.", sw.Elapsed, this.StreamId);
-                        }
-                    }
-                }
-                catch (Exception ex)
-                {
-                    this.logger.LogWarning(ex, "Last send did not finish successfully for stream {1}.", this.StreamId);
-                }
+                
+                this.Flush();
                 
                 this.closed = true;
             }

src/CsharpClient/QuixStreams.Streaming.UnitTests/Helpers/TestStreamingClient.cs
@@ -11,9 +11,10 @@ namespace QuixStreams.Streaming.UnitTests
         private TelemetryKafkaConsumer telemetryKafkaConsumer;
         private Func<string, TelemetryKafkaProducer> createKafkaProducer;
 
-        public TestStreamingClient(CodecType codec = CodecType.Protobuf)
+
+        public TestStreamingClient(CodecType codec = CodecType.Protobuf, TimeSpan publishDelay = default)
         {
-            this.testBroker = new TestBroker();
+            this.testBroker = new TestBroker(publishDelay: publishDelay);
 
             CodecRegistry.Register(codec);
         }

src/CsharpClient/QuixStreams.Streaming.UnitTests/StreamProducerShould.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.Runtime.Serialization;
 using FluentAssertions;
 using NSubstitute;
+using QuixStreams.Streaming.Models;
 using QuixStreams.Telemetry.Common.Test;
 using QuixStreams.Telemetry.Kafka;
 using QuixStreams.Telemetry.Models;
 using Xunit;
+using EventDefinition = QuixStreams.Telemetry.Models.EventDefinition;
+using ParameterDefinition = QuixStreams.Telemetry.Models.ParameterDefinition;
 
 namespace QuixStreams.Streaming.UnitTests.Telemetry
 {
@@ -352,6 +355,37 @@ namespace QuixStreams.Streaming.UnitTests.Telemetry
             // Assert
             interceptedEvents.Count.Should().Be(1);
             interceptedEvents[0].Should().BeEquivalentTo(inputEvents);
+        }
+        
+        [Fact]
+        public void Flush_Valid_ShouldWaitMessagesToBeSent()
+        {
+            // Arrange
+            var client = new TestStreamingClient(publishDelay: TimeSpan.FromMilliseconds(500));
+            var topicConsumer = client.GetTopicConsumer();
+
+            var releasedData = new List<TimeseriesData>();
+            topicConsumer.OnStreamReceived += (_, streamConsumer) =>
+            {
+                var buffer = streamConsumer.Timeseries.CreateBuffer();
+                buffer.OnDataReleased += (_, args) =>
+                {
+                    releasedData.Add(args.Data);
+                };
+            };
+            topicConsumer.Subscribe();
+            
+            var stream = client.GetTopicProducer().CreateStream();
+            stream.Timeseries.Buffer.BufferTimeout = null;
+            
+            // Act
+            stream.Timeseries.Buffer.Publish(
+                new TimeseriesData().AddTimestamp(DateTime.Now).AddValue("p1", 1).TimeseriesData);
+            
+            stream.Flush();
+            
+            // Assert
+            releasedData.Count.Should().Be(1);
         }
     }
 }

src/CsharpClient/QuixStreams.Streaming/IStreamProducer.cs
@@ -35,6 +35,11 @@ namespace QuixStreams.Streaming
         StreamEventsProducer Events { get; }
 
         /// <summary>
+        /// Flush the pending data to stream.  
+        /// </summary>
+        void Flush();
+
+        /// <summary>
         /// Close the stream and flush the pending data to stream.
         /// </summary>
         /// <param name="streamState">Stream closing state</param>

src/CsharpClient/QuixStreams.Telemetry.Common.Test/TestBroker.cs
@@ -16,11 +16,12 @@ namespace QuixStreams.Telemetry.Common.Test
         /// Initializes a new instance of <see cref="TestBroker"/>
         /// </summary>
         /// <param name="generateExceptions">The test broker generates exceptions when it receives data</param>
-        public TestBroker(bool generateExceptions = false)
+        /// <param name="publishDelay">Delay when publishing to simulate a real broker delay</param>
+        public TestBroker(bool generateExceptions = false, TimeSpan publishDelay = default)
         {
             this.Consumer = new TestBrokerConsumer();
             // Connection between Transport Input and Output simulating a real broker
-            this.Producer = new TestBrokerProducer(Consumer, generateExceptions); 
+            this.Producer = new TestBrokerProducer(Consumer, generateExceptions, publishDelay); 
         }
 
         /// <summary>
@@ -42,16 +43,21 @@ namespace QuixStreams.Telemetry.Common.Test
     {
         private readonly TestBrokerConsumer testConsumer;
         private readonly bool generateExceptions;
+        private readonly TimeSpan publishDelay;
 
-        public TestBrokerProducer(TestBrokerConsumer testConsumer, bool generateExceptions = false)
+        public TestBrokerProducer(TestBrokerConsumer testConsumer, bool generateExceptions = false, TimeSpan publishDelay = default)
         {
             this.testConsumer = testConsumer;
             this.generateExceptions = generateExceptions;
+            this.publishDelay = publishDelay;
         }
 
         public async Task Publish(Package package, CancellationToken cancellationToken = default)
         {
             if (generateExceptions) throw new Exception("Test broker generated exception.");
+            
+            await Task.Delay(this.publishDelay, cancellationToken); // Simulating a real broker delay
+            
             await this.testConsumer.Send(package); // Redirecting to Output all the messages arriving from the Input
         }
     }

src/PythonClient/src/quixstreams/streamproducer.py
@@ -179,6 +179,12 @@ class StreamProducer(object):
             self._streamEventsProducer = StreamEventsProducer(self._interop.get_Events())
         return self._streamEventsProducer
 
+    def flush(self):
+        """
+        Flushes the pending data to stream.
+        """
+        self._interop.Flush()
+
     def close(self, end_type: StreamEndType = StreamEndType.Closed):
         """
         Closes the stream and flushes the pending data to stream.

commit 304a8cf5e155583d3196442b1e34dcd1fa5840b3

Remove flushing logic after stream is closed (#117)
Fixing a bug on Stream.Close() introduced with https://github.com/quixio/quix-streams/pull/112

The bug was that Flush method on producers was called after they have been disposed.

src/CsharpClient/QuixStreams.Streaming/StreamProducer.cs
@@ -226,28 +226,8 @@ namespace QuixStreams.Streaming
             this.Properties.Flush();
             this.Timeseries.Flush();
             this.Events.Flush();
-            try
-            {
-                if (lastSendTask != null && !lastSendTask.IsCanceled && !lastSendTask.IsCompleted && !lastSendTask.IsFaulted)
-                {
-                    this.logger.LogTrace("Waiting for last message send for stream {1}.",  this.StreamId);
-                    var sw = Stopwatch.StartNew();
-                    Task.WaitAny(new[] {lastSendTask}, TimeSpan.FromSeconds(10));
-                    sw.Stop();
-                    if (!lastSendTask.IsCanceled && !lastSendTask.IsCompleted && !lastSendTask.IsFaulted)
-                    {
-                        this.logger.LogWarning("Last send did not finish in {0:g} for stream {1}. In future this timeout will be configurable.", sw.Elapsed, this.StreamId);
-                    }
-                    else
-                    {
-                        this.logger.LogTrace("Finished waiting for last message send in {0:g} for stream {1}.", sw.Elapsed, this.StreamId);
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                this.logger.LogWarning(ex, "Last send did not finish successfully for stream {1}.", this.StreamId);
-            }
+            
+            this.WaitForLastSendTaskToFinish();
         }        
 
         /// <inheritdoc />
@@ -279,9 +259,35 @@ namespace QuixStreams.Streaming
                 // Close stream
                 base.Close();
                 
-                this.Flush();
+                this.WaitForLastSendTaskToFinish();
                 
                 this.closed = true;
+            }
+        }
+
+        private void WaitForLastSendTaskToFinish()
+        {
+            try
+            {
+                if (lastSendTask != null && !lastSendTask.IsCanceled && !lastSendTask.IsCompleted && !lastSendTask.IsFaulted)
+                {
+                    this.logger.LogTrace("Waiting for last message send for stream {1}.",  this.StreamId);
+                    var sw = Stopwatch.StartNew();
+                    Task.WaitAny(new[] {lastSendTask}, TimeSpan.FromSeconds(10));
+                    sw.Stop();
+                    if (!lastSendTask.IsCanceled && !lastSendTask.IsCompleted && !lastSendTask.IsFaulted)
+                    {
+                        this.logger.LogWarning("Last send did not finish in {0:g} for stream {1}. In future this timeout will be configurable.", sw.Elapsed, this.StreamId);
+                    }
+                    else
+                    {
+                        this.logger.LogTrace("Finished waiting for last message send in {0:g} for stream {1}.", sw.Elapsed, this.StreamId);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                this.logger.LogWarning(ex, "Last send did not finish successfully for stream {1}.", this.StreamId);
             }
         }
 