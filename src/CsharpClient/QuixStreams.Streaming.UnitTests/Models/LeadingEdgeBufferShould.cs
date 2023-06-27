2e9b35b2bfeda6caea0d7c9f2f7a48c7867f56c4
Haris Botic <haris@quix.io>
2023-06-27 10:52:35 +0200

    Implement LeadingEdgeBuffer

---
+++ src/CsharpClient/QuixStreams.Streaming.UnitTests/Models/LeadingEdgeBufferShould.cs
@@ -0,0 +1,195 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using FluentAssertions;
+using NSubstitute;
+using Quix.TestBase.Extensions;
+using QuixStreams.Streaming.Models;
+using QuixStreams.Telemetry.Models;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace QuixStreams.Streaming.UnitTests.Models
+{
+    public class LeadingEdgeBufferShould
+    {
+        public LeadingEdgeBufferShould(ITestOutputHelper helper)
+        {
+            QuixStreams.Logging.Factory = helper.CreateLoggerFactory();
+        }
+        
+        [Fact]
+        public void WriteToBuffer_WithHighLeadingEdgeDelay_ShouldPublishDataCorrectly()
+        {
+            // Arrange
+            var topicProducer = Substitute.For<ITopicProducer>();
+            var streamProducer = Substitute.For<IStreamProducerInternal>();
+            
+            var timeseriesProducer = new QuixStreams.Streaming.Models.StreamProducer.StreamTimeseriesProducer(topicProducer, streamProducer);
+
+            var buffer = new LeadingEdgeBuffer(timeseriesProducer, 5000, 500);
+            
+            var publishedData = new List<TimeseriesDataRaw>();
+            var onDataReleasedRaiseCount = 0;
+            streamProducer.Publish(Arg.Do<TimeseriesDataRaw>(x=>
+            {
+                onDataReleasedRaiseCount++;
+                publishedData.Add(x);
+            }));
+
+
+            //Act
+            foreach (var timestampInMs in new []{1000, 1250, 1500, 1750, 2000, 2500, 9000})
+            {
+                buffer.GetOrCreateTimestamp(timestampInMs * (long)1e6).AddValue("ms_value", timestampInMs).Commit();
+            }
+            
+            onDataReleasedRaiseCount.Should().Be(3);
+            publishedData.Count.Should().Be(3); // (1000, 1250), (1500, 1750), 2000
+            publishedData[0].Should().BeEquivalentTo(CreateTSDataRawWithFixedTimestamp(1000, 1250));
+            publishedData[1].Should().BeEquivalentTo(CreateTSDataRawWithFixedTimestamp(1500, 1750));
+            publishedData[2].Should().BeEquivalentTo(CreateTSDataRawWithFixedTimestamp(2000));
+        }
+
+        [Fact]
+        public void WriteToBuffer_WithUnorderedTimestamps_ShouldPublishDataCorrectly()
+        {
+            // Arrange
+            var topicProducer = Substitute.For<ITopicProducer>();
+            var streamProducer = Substitute.For<IStreamProducerInternal>();
+            
+            var timeseriesProducer = new QuixStreams.Streaming.Models.StreamProducer.StreamTimeseriesProducer(topicProducer, streamProducer);
+
+            var buffer = new LeadingEdgeBuffer(timeseriesProducer, 1000, 500);
+            
+            var publishedData = new List<TimeseriesDataRaw>();
+            var onDataReleasedRaiseCount = 0;
+            streamProducer.Publish(Arg.Do<TimeseriesDataRaw>(x=>
+            {
+                onDataReleasedRaiseCount++;
+                publishedData.Add(x);
+            }));
+        
+        
+            //Act
+            foreach (var timestampInMs in new []{1250, 1000, 1300, 2000, 1400, 1750, 2500, 3000, 3250, 3000, 3300, 3500, 6000})
+            {
+                buffer.GetOrCreateTimestamp(timestampInMs * (long)1e6).AddValue("ms_value", timestampInMs).Commit(); 
+            }
+            
+            // Assert
+            onDataReleasedRaiseCount.Should().Be(4);
+            publishedData.Count.Should().Be(4); // (1000, 1250, 1300, 1400) + (1750, 2000), 2500, (3000, 3250, 3300)
+            publishedData[0].Should().BeEquivalentTo(CreateTSDataRawWithFixedTimestamp(1000, 1250, 1300, 1400));
+            publishedData[1].Should().BeEquivalentTo(CreateTSDataRawWithFixedTimestamp(1750, 2000));
+            publishedData[2].Should().BeEquivalentTo(CreateTSDataRawWithFixedTimestamp(2500));
+            publishedData[3].Should().BeEquivalentTo(CreateTSDataRawWithFixedTimestamp(3000, 3250, 3300));
+        
+        }
+        
+        [Fact]
+        public void WriteToBuffer_WithTimestampsArrivingTooLate_ShouldRaiseOnBackfillCorrectly()
+        {
+            // Arrange
+            var topicProducer = Substitute.For<ITopicProducer>();
+            var streamProducer = Substitute.For<IStreamProducerInternal>();
+            
+            var timeseriesProducer = new QuixStreams.Streaming.Models.StreamProducer.StreamTimeseriesProducer(topicProducer, streamProducer);
+
+            var buffer = new LeadingEdgeBuffer(timeseriesProducer, 1000, 500);
+            
+            var publishedData = new List<TimeseriesDataRaw>();
+            var onDataReleasedRaiseCount = 0;
+            streamProducer.Publish(Arg.Do<TimeseriesDataRaw>(x=>
+            {
+                onDataReleasedRaiseCount++;
+                publishedData.Add(x);
+            }));
+        
+            var backfilledData = new List<TimeseriesData>();
+            var onBackfillRaiseCount = 0;
+            buffer.OnBackfill += (sender, args) =>
+            {
+                backfilledData.Add(args.Data);
+                onBackfillRaiseCount++;
+            };
+        
+            //Act
+            foreach (var timestampInMs in new []{1000, 1250, 1500, 2500, 10, 20, 30})
+            {
+                buffer.GetOrCreateTimestamp(timestampInMs * (long)1e6).AddValue("ms_value", timestampInMs).Commit(); 
+            }
+            
+        
+            // Assert
+            onBackfillRaiseCount.Should().Be(3);
+            backfilledData.Count.Should().Be(3); // 10, 20, 30
+            backfilledData[0].ConvertToTimeseriesDataRaw().Should().BeEquivalentTo(CreateTSDataRawWithFixedTimestamp(10));
+            backfilledData[1].ConvertToTimeseriesDataRaw().Should().BeEquivalentTo(CreateTSDataRawWithFixedTimestamp(20));
+            backfilledData[2].ConvertToTimeseriesDataRaw().Should().BeEquivalentTo(CreateTSDataRawWithFixedTimestamp(30));
+        }
+        
+        
+        // [Theory]
+        // [InlineData(true)]
+        // [InlineData(false)]
+        // public void FlushData_WithLEDelayAndTimeSpanConfig_ShouldFlushCorrectly(bool initialConfig)
+        // {
+        //     // Arrange
+        //     var bufferConfiguration = GetEmptyTimeseriesBufferConfiguration();
+        //
+        //     if (initialConfig)
+        //     {
+        //         bufferConfiguration.LeadingEdgeDelay = 5000;
+        //         bufferConfiguration.TimeSpanInMilliseconds = 500;
+        //     }
+        //
+        //     var buffer = new TimeseriesBuffer(bufferConfiguration);
+        //     if (!initialConfig)
+        //     {
+        //         buffer.LeadingEdgeDelay = 5000;
+        //         buffer.TimeSpanInMilliseconds = 500;
+        //     }
+        //
+        //
+        //     var receivedData = new List<TimeseriesData>();
+        //     var onDataReleasedRaiseCount = 0;
+        //     buffer.OnDataReleased += (sender, args) =>
+        //     {
+        //         receivedData.Add(args.Data);
+        //         onDataReleasedRaiseCount++;
+        //     };
+        //
+        //
+        //     //Act
+        //     foreach (var timeseriesDataWithSingleTimestamp in new []{1000, 1250, 1500, 1750, 2000, 2500, 9000}.Select(x => CreateTimeseriesDataWithFixedTimestamp(x)))
+        //     {
+        //         buffer.WriteChunk(timeseriesDataWithSingleTimestamp.ConvertToTimeseriesDataRaw(false, false)); 
+        //     }
+        //
+        //     buffer.FlushData(false, includeDataInLeadingEdgeDelay: true);
+        //
+        //     // or
+        //     onDataReleasedRaiseCount.Should().Be(4);
+        //     receivedData.Count.Should().Be(4); // (1000, 1250), (1500, 1750), 2000, (2500, 9000)
+        //     receivedData[0].Should().BeEquivalentTo(CreateTimeseriesDataWithFixedTimestamp(1000, 1250));
+        //     receivedData[1].Should().BeEquivalentTo(CreateTimeseriesDataWithFixedTimestamp(1500, 1750));
+        //     receivedData[2].Should().BeEquivalentTo(CreateTimeseriesDataWithFixedTimestamp(2000));
+        //     receivedData[3].Should().BeEquivalentTo(CreateTimeseriesDataWithFixedTimestamp(2500, 9000));
+        //
+        // }
+        
+        private TimeseriesDataRaw CreateTSDataRawWithFixedTimestamp(params int[] msWithEpochArray)
+        {
+            var data = new TimeseriesData(capacity: msWithEpochArray.Length);
+            foreach (var msWithEpoch in msWithEpochArray)
+            {
+                data.AddTimestampNanoseconds(msWithEpoch * (long)1e6, epochIncluded: true)
+                    .AddValue("ms_value", msWithEpoch);
+            }
+
+            return data.ConvertToTimeseriesDataRaw();
+        }
+
+    }
+}
---
+++ src/CsharpClient/QuixStreams.Streaming/Models/LeadingEdgeBuffer.cs
@@ -0,0 +1,266 @@
+using System;
+using System.Collections.Generic;
+using QuixStreams.Streaming.Models.StreamConsumer;
+using QuixStreams.Streaming.Models.StreamProducer;
+
+namespace QuixStreams.Streaming.Models
+{
+    public class LeadingEdgeBuffer
+    {
+        private readonly SortedDictionary<(long, long), TimeseriesDataRow> rows;
+        private readonly StreamTimeseriesProducer producer;
+        private readonly long leadingEdgeDelayInNanoseconds;
+        private readonly long bucketInNanoseconds;
+
+        private long leadingEdgeInNanoseconds = Int64.MinValue;
+        private long lastTimestampReleased = Int64.MinValue;
+
+        /// <summary>
+        /// Data arriving with a timestamp earlier then the lastest released timestamp is discarded but released in this event for further processing or forwarding.
+        /// This event is invoked only if the buffer is configured with a leading edge delay.
+        /// </summary>
+        public event EventHandler<TimeseriesDataReadEventArgs> OnBackfill;
+        
+        /// <summary>
+        /// Initializes a new instance of <see cref="LeadingEdgeBuffer"/>
+        /// </summary>
+        /// <param name="producer">Instance of <see cref="StreamTimeseriesProducer"/></param>
+        /// <param name="leadingEdgeDelayMs">Leading edge delay configuration in Milliseconds</param>
+        /// <param name="bucketMs">Time between timestamps to wait for before publishing data in milliseconds</param>
+        public LeadingEdgeBuffer(StreamTimeseriesProducer producer, int leadingEdgeDelayMs, int bucketMs)
+        {
+            this.producer = producer;
+            this.leadingEdgeDelayInNanoseconds = leadingEdgeDelayMs * (long)1e6;
+            this.bucketInNanoseconds = bucketMs * (long)1e6;
+            this.rows = new SortedDictionary<(long, long), TimeseriesDataRow>();
+        }
+        
+        
+        /// <summary>
+        /// Gets an already buffered row based on timestamp and tags that can be modified or creates a new one if it doesn't exist.
+        /// </summary>
+        /// <param name="timestampInNanoseconds">Timestamp in nanoseconds</param>
+        /// <param name="tags">Optional Tags</param>
+        /// <returns></returns>
+        public TimeseriesDataRow GetOrCreateTimestamp(long timestampInNanoseconds, Dictionary<string, string> tags = null)
+        {
+            if (!rows.TryGetValue((timestampInNanoseconds, TagsHash(tags)), out var row))
+            {
+                row = new TimeseriesDataRow(this, timestampInNanoseconds, tags);
+            }
+
+            return row;
+        }
+
+        internal void WriteToBuffer(TimeseriesDataRow row)
+        {
+            if (rows.ContainsKey((row.Timestamp, TagsHash(row.Tags))))
+            {
+                return;
+            }
+
+            if (row.Timestamp <= this.lastTimestampReleased)
+            {
+                this.InvokeOnBackill(this, new TimeseriesDataReadEventArgs(null, null, row.AppendToTimeseriesData(new TimeseriesData())));
+                return;
+            }
+
+            rows[(row.Timestamp, TagsHash(row.Tags))] = row;
+            leadingEdgeInNanoseconds = Math.Max(this.leadingEdgeInNanoseconds, row.Timestamp);
+
+            EnsureFlushed();
+        }
+
+        private void EnsureFlushed()
+        {
+            var keysToPublish = new List<(long, long)>();
+            foreach (var row in rows)
+            {
+                var tsInNanoseconds = row.Key.Item1;
+
+                if (this.leadingEdgeInNanoseconds - tsInNanoseconds >= this.leadingEdgeDelayInNanoseconds)
+                {
+                    keysToPublish.Add(row.Key);
+                }
+            }
+
+            int startIndex = 0;
+            long minTimestamp = Int64.MaxValue;
+            long maxTimestamp = Int64.MinValue;
+            for (int i = 0; i < keysToPublish.Count; i++)
+            {
+                var nano = keysToPublish[i].Item1;
+
+                minTimestamp = Math.Min(minTimestamp, nano);
+                maxTimestamp = Math.Max(maxTimestamp, nano);
+
+                var nsDiff = maxTimestamp - minTimestamp;
+                if (nsDiff >= this.bucketInNanoseconds)
+                {
+                    // Reset min/max to this value, as will be flushing everything but this
+                    minTimestamp = nano;
+                    maxTimestamp = nano;
+
+                    Publish(keysToPublish.GetRange(startIndex, i - startIndex).ToArray());
+
+                    startIndex = i;
+                }
+            }
+        }
+
+        private void Publish(params (long, long)[] keys)
+        {
+            var timeseriesData = new TimeseriesData(capacity: keys.Length);
+            foreach (var key in keys)
+            {
+                var timestampInNanoseconds = key.Item1;
+                var row = rows[key];
+
+                timeseriesData = row.AppendToTimeseriesData(timeseriesData);
+
+                this.lastTimestampReleased = Math.Max(this.lastTimestampReleased, timestampInNanoseconds);
+                rows.Remove(key);
+            }
+
+            producer.Publish(timeseriesData);
+        }
+
+        private void InvokeOnBackill(object sender, TimeseriesDataReadEventArgs args)
+        {
+            this.OnBackfill?.Invoke(this, args);
+        }
+
+        private static long TagsHash(Dictionary<string, string> tags)
+        {
+            if (tags == null || tags.Count == 0) return 0;
+            unchecked
+            {
+                var hash = 397;
+                foreach (var kpair in tags)
+                {
+                    hash ^= kpair.Value?.GetHashCode() ?? 0;
+                    hash ^= kpair.Key.GetHashCode();
+                }
+
+                return hash;
+            }
+        }
+    }
+
+    /// <summary>
+    /// Represents a single row of data in the <see cref="LeadingEdgeBuffer"/>
+    /// </summary>
+    public class TimeseriesDataRow
+    {
+        internal long Timestamp { get; }
+        internal Dictionary<string, double> NumericValues { get; }
+        internal Dictionary<string, string> StringValues { get; }
+        internal Dictionary<string, byte[]> BinaryValues { get; }
+        internal Dictionary<string, string> Tags { get; }
+
+        private readonly LeadingEdgeBuffer buffer;
+        private bool committed = false;
+
+        public TimeseriesDataRow(LeadingEdgeBuffer buffer, long timestamp, Dictionary<string, string> tags)
+        {
+            this.buffer = buffer;
+            this.Timestamp = timestamp;
+            this.Tags = tags;
+
+            NumericValues = new Dictionary<string, double>();
+            StringValues = new Dictionary<string, string>();
+            BinaryValues = new Dictionary<string, byte[]>();
+        }
+
+        /// <summary>
+        /// Adds a value to the row
+        /// </summary>
+        /// <param name="parameter">Parameter name</param>
+        /// <param name="value">Value of the parameter</param>
+        /// <param name="overwrite">If set to true, it will overwrite an existing value for the specified parameter if one already exists.
+        /// If set to false and a value for the specified parameter already exists, the method ignore the new value and just return the current TimeseriesDataRow instance.</param>
+        /// <returns>Returns the current TimeseriesDataRow instance. This allows for method chaining</returns>
+        public TimeseriesDataRow AddValue(string parameter, double value, bool overwrite = false)
+        {
+            if (NumericValues.ContainsKey(parameter) && !overwrite)
+            {
+                return this;
+            }
+
+            NumericValues[parameter] = value;
+            return this;
+        }
+
+        /// <summary>
+        /// Adds a value to the row
+        /// </summary>
+        /// <param name="parameter">Parameter name</param>
+        /// <param name="value">Value of the parameter</param>
+        /// <param name="overwrite">If set to true, it will overwrite an existing value for the specified parameter if one already exists.
+        /// If set to false and a value for the specified parameter already exists, the method ignore the new value and just return the current TimeseriesDataRow instance.</param>
+        /// <returns>Returns the current TimeseriesDataRow instance. This allows for method chaining</returns>
+        public TimeseriesDataRow AddValue(string parameter, string value, bool overwrite = false)
+        {
+            if (StringValues.ContainsKey(parameter) && !overwrite)
+            {
+                return this;
+            }
+
+            StringValues[parameter] = value;
+            return this;
+        }
+
+        /// <summary>
+        /// Adds a value to the row
+        /// </summary>
+        /// <param name="parameter">Parameter name</param>
+        /// <param name="value">Value of the parameter</param>
+        /// <param name="overwrite">If set to true, it will overwrite an existing value for the specified parameter if one already exists.
+        /// If set to false and a value for the specified parameter already exists, the method ignore the new value and just return the current TimeseriesDataRow instance.</param>
+        public TimeseriesDataRow AddValue(string parameter, byte[] value, bool overwrite = false)
+        {
+            if (BinaryValues.ContainsKey(parameter) && !overwrite)
+            {
+                return this;
+            }
+
+            BinaryValues[parameter] = value;
+            return this;
+        }
+
+        internal TimeseriesData AppendToTimeseriesData(TimeseriesData existingTimeseriesData)
+        {
+            var ts = existingTimeseriesData.AddTimestampNanoseconds(Timestamp, epochIncluded: true).AddTags(Tags);
+            foreach (var numericValue in NumericValues)
+            {
+                ts.AddValue(numericValue.Key, numericValue.Value);
+            }
+
+            foreach (var stringValue in StringValues)
+            {
+                ts.AddValue(stringValue.Key, stringValue.Value);
+            }
+
+            foreach (var binaryValue in BinaryValues)
+            {
+                ts.AddValue(binaryValue.Key, binaryValue.Value);
+            }
+
+            return existingTimeseriesData;
+        }
+        
+        /// <summary>
+        /// This method commits the current object instance to the <see cref="LeadingEdgeBuffer"/>.
+        /// </summary>
+        public void Commit()
+        {
+            if (committed)
+            {
+                return;
+            }
+
+            committed = true;
+            buffer.WriteToBuffer(this);
+        }
+    }
+}
