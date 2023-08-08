89eeca8b6fabe5b85bec3d16a5ad089262738998
Peter Nagy <peter@quix.io>
2023-08-08 18:14:14 +0100

    bump version

--- src/builds/csharp/nuget/build_nugets.py
+++ src/builds/csharp/nuget/build_nugets.py
@@ -7,8 +7,8 @@ import fileinput
 from typing import List
 
 version = "0.5.5.0"
-informal_version = "0.5.5.0-dev9"
-nuget_version = "0.5.5.0-dev9"
+informal_version = "0.5.5.0-10"
+nuget_version = "0.5.5.0-dev10"
 
 
 def updatecsproj(projfilepath):
