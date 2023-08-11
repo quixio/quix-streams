b7ea7a9a82e614cd060e88b99926a871b5c7b274
Tim Sawicki <tim@quix.io>
2023-08-10 21:48:28 -0400

    small update to Row, pipeline clones Rows and itself where needed. Pipeline handles basic ops and filtering

--- src/PythonClient/src/quixstreams/dataframes/models/rows.py
+++ src/PythonClient/src/quixstreams/dataframes/models/rows.py
@@ -1,11 +1,10 @@
 import dataclasses
 from typing import Mapping, Optional
-
 from .timestamps import MessageTimestamp
 from .types import MessageKey, MessageHeaders
 
 
-@dataclasses.dataclass(eq=True, kw_only=True, slots=True)
+@dataclasses.dataclass(eq=True, kw_only=True, slots=True, frozen=True)
 class Row:
     """
     Row is a dict-like interface on top of the message data + some Kafka props
@@ -21,3 +20,15 @@ class Row:
     headers: Optional[Mapping | MessageHeaders] = None
     latency: Optional[float] = None
     leader_epoch: Optional[int] = None
+
+    def __getitem__(self, item):
+        return self.value[item]
+
+    def __bool__(self):
+        return bool(self.value)
+
+    def items(self):
+        return self.value.items()
+
+    def _clone(self, **kwargs):
+        return Row(**{**dataclasses.asdict(self), **kwargs})
