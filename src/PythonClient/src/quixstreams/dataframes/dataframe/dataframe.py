898850036631dee26419b60bc01fdf9f294967c4
Daniil Gusev <daniil@quix.io>
2023-08-21 10:49:44 +0200

    Update StreamingDataFrame class
    - Inject Consumer and Producer to SDF
    - Temporarily prohibit using one SDF to filter another due until we figure out how to copy rows between the two

--- src/PythonClient/src/quixstreams/dataframes/dataframe/dataframe.py
+++ src/PythonClient/src/quixstreams/dataframes/dataframe/dataframe.py
@@ -1,18 +1,17 @@
 import uuid
 from copy import deepcopy
 from functools import partial
-from typing import Self, Optional, Callable, TypeAlias, Union
+from typing import Self, Optional, Callable, TypeAlias, Union, List, Mapping
 
 from .column import Column, OpValue
 from .pipeline import Pipeline, get_func_name
-from ..models.rows import Row
+from ..models import Row, Topic
+from ..rowconsumer import RowConsumerProto
+from ..rowproducer import RowProducerProto
 
 RowApplier: TypeAlias = Callable[[Row], Optional[Union[Row, list[Row]]]]
 
-
-__all__ = (
-    "StreamingDataFrame"
-)
+__all__ = ("StreamingDataFrame",)
 
 
 def subset(keys: list[str], row: Row) -> Row:
@@ -22,7 +21,6 @@ def subset(keys: list[str], row: Row) -> Row:
 
 
 def setitem(k: str, v: Union[Column, OpValue], row: Row) -> Row:
-    row = deepcopy(row)
     row[k] = v.eval(row) if isinstance(v, Column) else v
     return row
 
@@ -31,55 +29,21 @@ def column_filter(column: Column, row: Row) -> Optional[Row]:
     return row if column.eval(row) else None
 
 
-def row_apply(func: RowApplier, row: Row) -> Union[Row, list[Row], None]:
-    return func(deepcopy(row))
-
-
 # TODO: make a pipeline merge function to avoid accessing private methods
 class StreamingDataFrame:
-    def __init__(self, name: str = None, _pipeline: Pipeline = None):
+    def __init__(
+        self, topics: List[Topic], name: str = None, _pipeline: Pipeline = None
+    ):
         self.name = name or str(uuid.uuid4())
         self._pipeline = _pipeline or Pipeline(name=self.name)
-        self._producer: None
-
-    @property
-    def producer(self):
-        return None
-
-    def get_topics(self):
-        ...
-
-    def set_producer(self):
-        ...
-
-    def to_topic(self):
-        ...
-
-    def _produce(self):
-        ...
-
-    def __setitem__(self, key: str, value: Union[Column, OpValue, str]):
-        self._apply(partial(setitem, key, value))
-
-    def __getitem__(
-            self, item: Union[str, list[str], Column, Self]
-    ) -> Union[Column, Self]:
-        if isinstance(item, (Column, StreamingDataFrame)):
-            return self._apply(self._filter(item))
-        elif isinstance(item, list):
-            return self._apply(partial(subset, item))
-        else:
-            return Column(col_name=item)
-
-    def _clone(self):
-        return self.__class__(_pipeline=self._pipeline.clone())
-
-    def _apply(self, func: RowApplier, func_name=None):
-        self._pipeline.apply(func, func_name=func_name)
-        return self
+        self._real_consumer: Optional[RowConsumerProto] = None
+        self._real_producer: Optional[RowProducerProto] = None
+        if not topics:
+            raise ValueError("Topic list cannot be empty")
+        self._topics = {t.name: t for t in topics}
 
     def apply(
-            self, func: Callable[[Row], Optional[Union[Row, list[Row], None]]]
+        self, func: Callable[[Row], Optional[Union[Row, list[Row], None]]]
     ) -> Self:
         """
         Add a function to the StreamingDataframe execution list.
@@ -90,20 +54,7 @@ class StreamingDataFrame:
         :param func: callable that accepts and (usually) returns a QuixStreams Row
         :return: self (StreamingDataFrame)
         """
-        return self._clone()._apply(
-            func=partial(row_apply, func),
-            func_name=f'apply:{get_func_name(func)}'
-        )
-
-    def _dataframe_filter(self, other_dataframe: Self, row: Row) -> Optional[Row]:
-        other_dataframe._pipeline.remove_redundant_functions(self._pipeline)
-        return row if other_dataframe.process(row) else None
-
-    def _filter(self, item: Union[Column, Self]) -> RowApplier:
-        if isinstance(item, Column):
-            return partial(column_filter, item)
-        else:
-            return partial(self._dataframe_filter, item)
+        return self._clone()._apply(func=func, func_name=f"apply:{get_func_name(func)}")
 
     def process(self, row: Row) -> Optional[Union[Row, list[Row]]]:
         """
@@ -112,3 +63,58 @@ class StreamingDataFrame:
         :return: Row, list of Rows, or None (if filtered)
         """
         return self._pipeline.process(row)
+
+    @property
+    def topics(self) -> Mapping[str, Topic]:
+        """
+        Get a mapping with Topics for the StreamingDataFrame
+        :return: dict of {<topic_name>: <Topic>}
+        """
+        return self._topics
+
+    @property
+    def consumer(self) -> RowConsumerProto:
+        if self._real_consumer is None:
+            raise RuntimeError("Consumer instance has not been provided")
+        return self._real_consumer
+
+    @consumer.setter
+    def consumer(self, consumer: RowConsumerProto):
+        self._real_consumer = consumer
+
+    @property
+    def producer(self) -> RowProducerProto:
+        if self._real_producer is None:
+            raise RuntimeError("Producer instance has not been provided")
+        return self._real_producer
+
+    @producer.setter
+    def producer(self, producer: RowProducerProto):
+        self._real_producer = producer
+
+    def __setitem__(self, key: str, value: Union[Column, OpValue, str]):
+        self._apply(partial(setitem, key, value))
+
+    def __getitem__(
+        self, item: Union[str, list[str], Column, Self]
+    ) -> Union[Column, Self]:
+        if isinstance(item, Column):
+            return self._apply(partial(column_filter, item))
+        elif isinstance(item, list):
+            return self._apply(partial(subset, item))
+        elif isinstance(item, StreamingDataFrame):
+            # TODO: Implement filtering based on another SDF
+            raise ValueError(
+                "Filtering based on StreamingDataFrame is not supported yet."
+            )
+        else:
+            return Column(col_name=item)
+
+    def _clone(self) -> Self:
+        return self.__class__(
+            topics=list(self._topics.values()), _pipeline=self._pipeline.clone()
+        )
+
+    def _apply(self, func: RowApplier, func_name=None):
+        self._pipeline.apply(func, func_name=func_name)
+        return self
--- src/PythonClient/tests/quixstreams/test_dataframes/test_dataframe/test_dataframe.py
+++ src/PythonClient/tests/quixstreams/test_dataframes/test_dataframe/test_dataframe.py
@@ -2,13 +2,13 @@ import pytest
 
 from src.quixstreams.dataframes.dataframe.pipeline import (
     InvalidPipelineBranching,
-    Pipeline
+    Pipeline,
 )
 
 
 class TestDataframe:
     def test_dataframe(self, dataframe):
-        assert dataframe.name == 'test_dataframe'
+        assert dataframe.name == "test_dataframe"
         assert isinstance(dataframe._pipeline, Pipeline)
         assert dataframe._pipeline.name == dataframe.name
 
@@ -20,27 +20,33 @@ class TestDataframe:
     def test_apply(self, dataframe):
         def test_func(row):
             return row
+
         dataframe2 = dataframe.apply(test_func)
         assert id(dataframe) != id(dataframe2)
-        assert dataframe2._pipeline.functions[-1].name == 'apply:test_func'
+        assert dataframe2._pipeline.functions[-1].name == "apply:test_func"
 
 
 class TestDataframeProcess:
-    def test_row_apply(
-            self, dataframe, row_with_ints, row_plus_n_func, row_plus_n
-    ):
+    def test_row_apply(self, dataframe, row_with_ints, row_plus_n_func, row_plus_n):
         n = 1
         dataframe = dataframe.apply(row_plus_n_func(n))
-        assert dataframe.process(row_with_ints) == row_plus_n(n, row_with_ints)
+        expected = row_plus_n(n, row_with_ints)
+        assert dataframe.process(row_with_ints) == expected
 
     def test_row_apply_fluent(
-            self, dataframe, row_with_ints, row_plus_n_func, row_plus_n
+        self,
+        dataframe,
+        row_with_ints,
+        row_plus_n_func,
+        row_plus_n,
+        row_msg_value_factory,
     ):
         dataframe = dataframe.apply(row_plus_n_func(1)).apply(row_plus_n_func(2))
-        assert dataframe.process(row_with_ints) == row_plus_n(3, row_with_ints)
+        expected = row_plus_n(3, row_with_ints)
+        assert dataframe.process(row_with_ints) == expected
 
     def test_row_apply_sequential(
-            self, dataframe, row_with_ints, row_plus_n_func, row_plus_n
+        self, dataframe, row_with_ints, row_plus_n_func, row_plus_n
     ):
         dataframe = dataframe.apply(row_plus_n_func(1))
         dataframe = dataframe.apply(row_plus_n_func(2))
@@ -49,63 +55,69 @@ class TestDataframeProcess:
         assert actual == expected
 
     def test_set_item_primitive(
-            self, dataframe, row_with_ints, msg_value_with_ints, row_msg_value_factory
+        self, dataframe, row_with_ints, msg_value_with_ints, row_msg_value_factory
     ):
-        dataframe['new_val'] = 1
-        expected = row_msg_value_factory({
-            **msg_value_with_ints,
-            'new_val': 1
-        })
+        dataframe["new_val"] = 1
+        expected = row_msg_value_factory({**msg_value_with_ints, "new_val": 1})
         actual = dataframe.process(row_with_ints)
         assert actual == expected
 
     def test_set_item_column_only(
-            self, dataframe, row_with_ints, msg_value_with_ints, row_msg_value_factory
+        self, dataframe, row_with_ints, msg_value_with_ints, row_msg_value_factory
     ):
-        dataframe['new_val'] = dataframe['x']
-        expected = row_msg_value_factory({
-            **msg_value_with_ints,
-            'new_val': msg_value_with_ints['x']
-        })
+        dataframe["new_val"] = dataframe["x"]
+        expected = row_msg_value_factory(
+            {**msg_value_with_ints, "new_val": msg_value_with_ints["x"]}
+        )
         actual = dataframe.process(row_with_ints)
         assert actual == expected
 
     def test_set_item_column_with_function(
-            self, dataframe, row_with_ints, msg_value_with_ints, row_msg_value_factory
+        self, dataframe, row_with_ints, msg_value_with_ints, row_msg_value_factory
     ):
-        dataframe['new_val'] = dataframe['x'].apply(lambda v: v + 10)
-        expected = row_msg_value_factory({
-            **msg_value_with_ints,
-            'new_val': msg_value_with_ints['x'] + 10
-        })
+        dataframe["new_val"] = dataframe["x"].apply(lambda v: v + 10)
+        expected = row_msg_value_factory(
+            {**msg_value_with_ints, "new_val": msg_value_with_ints["x"] + 10}
+        )
         actual = dataframe.process(row_with_ints)
         assert actual == expected
 
     def test_set_item_column_with_operations(
-            self, dataframe, row_with_ints, msg_value_with_ints, row_msg_value_factory
+        self, dataframe, row_with_ints, msg_value_with_ints, row_msg_value_factory
     ):
-        dataframe['new_val'] = dataframe['x'] + dataframe['y'].apply(lambda v: v + 10) + 1
-        expected = row_msg_value_factory({
-            **msg_value_with_ints,
-            'new_val': msg_value_with_ints['x'] + (msg_value_with_ints['y'] + 10) + 1})
+        dataframe["new_val"] = (
+            dataframe["x"] + dataframe["y"].apply(lambda v: v + 10) + 1
+        )
+        expected = row_msg_value_factory(
+            {
+                **msg_value_with_ints,
+                "new_val": msg_value_with_ints["x"]
+                + (msg_value_with_ints["y"] + 10)
+                + 1,
+            }
+        )
         actual = dataframe.process(row_with_ints)
         assert actual == expected
 
     def test_column_subset(
-            self, dataframe, row_with_ints, msg_value_with_ints, row_msg_value_factory
+        self, dataframe, row_with_ints, msg_value_with_ints, row_msg_value_factory
     ):
-        key_list = ['x', 'y']
+        key_list = ["x", "y"]
         dataframe = dataframe[key_list]
         expected = row_msg_value_factory({k: msg_value_with_ints[k] for k in key_list})
         actual = dataframe.process(row_with_ints)
         assert actual == expected
 
     def test_column_subset_with_funcs(
-            self, dataframe, row_with_ints, msg_value_with_ints,
-            row_plus_n_func, row_msg_value_factory
+        self,
+        dataframe,
+        row_with_ints,
+        msg_value_with_ints,
+        row_plus_n_func,
+        row_msg_value_factory,
     ):
         n = 1
-        key_list = ['x', 'y']
+        key_list = ["x", "y"]
         dataframe = dataframe[key_list].apply(row_plus_n_func(n))
         expected = row_msg_value_factory(
             {k: msg_value_with_ints[k] + n for k in key_list}
@@ -114,73 +126,94 @@ class TestDataframeProcess:
         assert actual == expected
 
     def test_inequality_filtering(self, dataframe, row_with_ints):
-        dataframe = dataframe[dataframe['x'] > 0]
+        dataframe = dataframe[dataframe["x"] > 0]
         assert dataframe.process(row_with_ints) == row_with_ints
 
     def test_inequality_filtering_with_operation(self, dataframe, row_with_ints):
-        dataframe = dataframe[(dataframe['x'] - 0 + dataframe['y']) > 0]
+        dataframe = dataframe[(dataframe["x"] - 0 + dataframe["y"]) > 0]
         assert dataframe.process(row_with_ints) == row_with_ints
 
     def test_inequality_filtering_with_operation_filtered(
-            self, dataframe, row_with_ints
+        self, dataframe, row_with_ints
     ):
-        dataframe = dataframe[(dataframe['x'] - dataframe['y']) > 0]
+        dataframe = dataframe[(dataframe["x"] - dataframe["y"]) > 0]
         assert dataframe.process(row_with_ints) is None
 
     def test_inequality_filtering_with_apply_filtered(self, dataframe, row_with_ints):
-        dataframe = dataframe[dataframe['x'].apply(lambda v: v - 1000) >= 0]
+        dataframe = dataframe[dataframe["x"].apply(lambda v: v - 1000) >= 0]
         assert dataframe.process(row_with_ints) is None
 
     def test_inequality_filtering_filtered(self, dataframe, row_with_ints):
-        dataframe = dataframe[dataframe['x'] >= 1000]
+        dataframe = dataframe[dataframe["x"] >= 1000]
         assert dataframe.process(row_with_ints) is None
 
     def test_compound_inequality_filtering_continue(self, dataframe, row_with_ints):
-        dataframe = dataframe[(dataframe['x'] >= 0) & (dataframe['y'] < 100)]
+        dataframe = dataframe[(dataframe["x"] >= 0) & (dataframe["y"] < 100)]
         assert dataframe.process(row_with_ints) == row_with_ints
 
     def test_compound_inequality_filtering_filtered(self, dataframe, row_with_ints):
-        dataframe = dataframe[(dataframe['x'] >= 0) & (dataframe['y'] < 0)]
+        dataframe = dataframe[(dataframe["x"] >= 0) & (dataframe["y"] < 0)]
         assert dataframe.process(row_with_ints) is None
 
+    @pytest.mark.skip("SDF-based filtering is not supported yet")
+    def test_row_apply_filtering_fails(self, dataframe, row_with_ints):
+        dataframe = dataframe[
+            dataframe.apply(lambda row: row if row["x"] > 0 else None)
+        ]
+        with pytest.raises(
+            ValueError,
+            match="Filtering based on StreamingDataFrame is not supported yet",
+        ):
+            assert dataframe.process(row_with_ints) == row_with_ints
+
+    @pytest.mark.skip("SDF-based filtering is not supported yet")
     def test_row_apply_filtering_continue(self, dataframe, row_with_ints):
         dataframe = dataframe[
-            dataframe.apply(lambda row: row if row['x'] > 0 else None)
+            dataframe.apply(lambda row: row if row["x"] > 0 else None)
         ]
         assert dataframe.process(row_with_ints) == row_with_ints
 
+    @pytest.mark.skip("SDF-based filtering is not supported yet")
     def test_row_apply_filtering_filter_none(self, dataframe, row_with_ints):
         dataframe = dataframe[
-            dataframe.apply(lambda row: row if row['x'] < 0 else None)
+            dataframe.apply(lambda row: row if row["x"] < 0 else None)
         ]
         assert dataframe.process(row_with_ints) is None
 
+    @pytest.mark.skip("SDF-based filtering is not supported yet")
     def test_row_apply_filtering_filter_false(self, dataframe, row_with_ints):
         dataframe = dataframe[dataframe.apply(lambda row: False)]
         assert dataframe.process(row_with_ints) is None
 
+    @pytest.mark.skip("SDF-based filtering is not supported yet")
     def test_row_apply_filtering_sequential_continue(self, dataframe, row_with_ints):
-        p_filter = dataframe.apply(lambda row: row if row['x'] > 0 else None)
+        p_filter = dataframe.apply(lambda row: row if row["x"] > 0 else None)
         dataframe = dataframe[p_filter]
         assert dataframe.process(row_with_ints) == row_with_ints
 
+    @pytest.mark.skip("Requires copying, not implemented yet")
     def test_row_apply_filtering_dataframe_uses_apply_in_filter(
-            self, dataframe, row_with_ints, row_plus_n_func, row_plus_n):
+        self, dataframe, row_with_ints, row_plus_n_func, row_plus_n
+    ):
         n = 1
         dataframe = dataframe.apply(row_plus_n_func(n))
         dataframe = dataframe[dataframe.apply(lambda row: row)]
         assert dataframe.process(row_with_ints) == row_plus_n(n, row_with_ints)
 
+    @pytest.mark.skip("Requires copying, not implemented yet")
     def test_row_apply_filtering_separate_filtering_dataframe(
-            self, dataframe, row_with_ints, row_plus_n_func, row_plus_n):
+        self, dataframe, row_with_ints, row_plus_n_func, row_plus_n
+    ):
         dataframe = dataframe.apply(row_plus_n_func(1))
         # note, filter should NOT apply to final dataframe result!
         filter_dataframe = dataframe.apply(row_plus_n_func(2))
         dataframe = dataframe[filter_dataframe.apply(lambda row: row)]
         assert dataframe.process(row_with_ints) == row_plus_n(1, row_with_ints)
 
+    @pytest.mark.skip("SDF-based filtering is not supported yet")
     def test_row_apply_filtering_parent_child_divergence_raises_invalid(
-            self, dataframe, row_with_ints, row_plus_n_func):
+        self, dataframe, row_with_ints, row_plus_n_func
+    ):
         """
         This is invalid due to the "parent" dataframe adding new functions after
         previously generating a child that is later used as the parent's filter.
@@ -194,7 +227,7 @@ class TestDataframeProcess:
             dataframe = dataframe[filter_dataframe.apply(lambda row: row)]
             dataframe.process(row_with_ints)
 
-    @pytest.mark.skip('This should fail based on our outline but currently does not')
+    @pytest.mark.skip("This should fail based on our outline but currently does not")
     # TODO: make this fail correctly
     def test_non_row_apply_breaks_things(self, dataframe, row_with_ints):
         dataframe = dataframe.apply(lambda row: False)
@@ -202,42 +235,55 @@ class TestDataframeProcess:
         dataframe.process(row_with_ints)
 
     def test_multiple_row_generation(
-            self, dataframe, more_rows_func, row_msg_value_factory,
-            row_with_list, msg_value_with_list,
+        self,
+        dataframe,
+        more_rows_func,
+        row_msg_value_factory,
+        row_with_list,
+        msg_value_with_list,
     ):
         dataframe = dataframe.apply(more_rows_func)
         expected = [
-            row_msg_value_factory({**msg_value_with_list, 'x_list': v})
-            for v in msg_value_with_list['x_list']
+            row_msg_value_factory({**msg_value_with_list, "x_list": v})
+            for v in msg_value_with_list["x_list"]
         ]
         actual = dataframe.process(row_with_list)
         assert actual == expected
 
     def test_multiple_row_generation_with_additional_apply(
-            self, dataframe, more_rows_func, row_msg_value_factory,
-            row_with_list, msg_value_with_list,
+        self,
+        dataframe,
+        more_rows_func,
+        row_msg_value_factory,
+        row_with_list,
+        msg_value_with_list,
     ):
         def add_to_xlist(row):
-            row['x_list'] += 1
+            row["x_list"] += 1
             return row
+
         dataframe = dataframe.apply(more_rows_func)
         dataframe = dataframe.apply(add_to_xlist)
         expected = [
-            row_msg_value_factory({**msg_value_with_list, 'x_list': v + 1})
-            for v in msg_value_with_list['x_list']
+            row_msg_value_factory({**msg_value_with_list, "x_list": v + 1})
+            for v in msg_value_with_list["x_list"]
         ]
         actual = dataframe.process(row_with_list)
         assert actual == expected
 
     def test_multiple_row_generation_with_additional_filtering(
-            self, dataframe, more_rows_func, row_msg_value_factory,
-            row_with_list, msg_value_with_list
+        self,
+        dataframe,
+        more_rows_func,
+        row_msg_value_factory,
+        row_with_list,
+        msg_value_with_list,
     ):
         dataframe = dataframe.apply(more_rows_func)
-        dataframe = dataframe.apply(lambda row: row if row['x_list'] > 0 else None)
+        dataframe = dataframe.apply(lambda row: row if row["x_list"] > 0 else None)
         expected = [
-            row_msg_value_factory({**msg_value_with_list, 'x_list': v})
-            for v in msg_value_with_list['x_list'][1:]
+            row_msg_value_factory({**msg_value_with_list, "x_list": v})
+            for v in msg_value_with_list["x_list"][1:]
         ]
         actual = dataframe.process(row_with_list)
         assert actual == expected
