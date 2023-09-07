9d269817c1e9e1ce4c8842cfd9a76dcf66d3216f
Tim Sawicki <tim@quix.io>
2023-09-07 12:54:40 -0400

    remove unneeded fixtures

--- src/PythonClient/tests/quixstreams/test_dataframes/test_platforms/test_quix/fixtures.py
+++ src/PythonClient/tests/quixstreams/test_dataframes/test_platforms/test_quix/fixtures.py
@@ -6,29 +6,8 @@ from src.quixstreams.dataframes.platforms.quix.api import QuixPortalApiService
 from src.quixstreams.dataframes.platforms.quix.config import QuixKafkaConfigsBuilder
 
 
-class MockResponse:
-    def __init__(self, data, status_code):
-        self.data = data
-        self.status_code = status_code
-
-    def json(self):
-        return self.data
-
-    @property
-    def content(self):
-        return self.data
-
-
 @pytest.fixture()
-def mock_response_factory():
-    def mock_response(json_data, status_code=200):
-        return MockResponse(json_data, status_code)
-
-    return mock_response
-
-
-@pytest.fixture()
-def mock_quix_portal_api_factory(mock_response_factory):
+def mock_quix_portal_api_factory():
     def mock_quix_portal_api(api_responses: dict = None):
         if not api_responses:
             api_responses = {}
