0460a86e61df6e401d58d9a06da47b6345d2abcd
tim-quix <136370015+tim-quix@users.noreply.github.com>
2023-09-20 09:03:30 -0400

    first push of API stuff (#181)
    * first push of API stuff
    
    * oops idk how that happened lul
    
    * moved stuff around, added a workspace search (for v1), lots of documentation and cleanup
    
    * remove unneeded fixtures
    
    * confirmed topic creation works, get_workspaces w/PAT does not
    
    * updates based on code review
    
    * updates based on code review and prod connection
    
    * fix v1 workspace and add a test for it

---
+++ src/PythonClient/src/quixstreams/dataframes/platforms/quix/api.py
@@ -0,0 +1,131 @@
+from io import BytesIO
+from os import environ
+from typing import Optional
+from urllib.parse import urljoin
+from zipfile import ZipFile
+
+import requests
+
+__all__ = ("QuixPortalApiService",)
+
+
+class QuixPortalApiService:
+    """
+    A light wrapper around the Quix Portal Api. If used in the Quix Platform, it will
+    use that workspaces auth token and portal endpoint, else you must provide it.
+
+    Function names closely reflect the respective API endpoint,
+    each starting with the method [GET, POST, etc] followed by the endpoint path.
+
+    Results will be returned in the form of request's Response.json(), unless something
+    else is required. Non-200's will raise exceptions.
+
+    See the swagger documentation for more info about the endpoints.
+    """
+
+    def __init__(
+        self,
+        portal_api: Optional[str] = None,
+        auth_token: Optional[str] = None,
+        api_version: Optional[str] = None,
+        default_workspace_id: Optional[str] = None,
+    ):
+        self._portal_api = portal_api or environ.get(self.QuixEnvironmentMap.portal_api)
+        self._auth_token = auth_token or environ.get(self.QuixEnvironmentMap.sdk_token)
+        self.default_workspace_id = default_workspace_id or environ.get(
+            self.QuixEnvironmentMap.workspace_id
+        )
+        self.api_version = api_version or "2.0"
+        self.session = self._init_session()
+
+    class MissingConnectionRequirements(Exception):
+        pass
+
+    class QuixEnvironmentMap:
+        sdk_token = "Quix__Sdk__Token"
+        portal_api = "Quix__Portal__Api"
+        workspace_id = "Quix__Workspace__Id"
+
+    class SessionWithUrlBase(requests.Session):
+        def __init__(self, url_base: str):
+            self.url_base = url_base
+            super().__init__()
+
+        def request(self, method, url, **kwargs):
+            return super().request(
+                method, urljoin(base=self.url_base, url=url), **kwargs
+            )
+
+    def _init_session(self) -> SessionWithUrlBase:
+        if not (self._portal_api and self._auth_token):
+            missing = filter(
+                None,
+                [self._portal_api or "portal_api", self._auth_token or "auth_token"],
+            )
+            raise self.MissingConnectionRequirements(
+                f"Using the API requires a portal and token. Missing: {missing}"
+            )
+        s = self.SessionWithUrlBase(self._portal_api)
+        s.hooks = {"response": lambda r, *args, **kwargs: r.raise_for_status()}
+        s.headers.update(
+            {
+                "X-Version": self.api_version,
+                "Authorization": f"Bearer {self._auth_token}",
+            }
+        )
+        return s
+
+    def get_workspace_certificate(self, workspace_id: Optional[str] = None) -> bytes:
+        if not workspace_id:
+            workspace_id = self.default_workspace_id
+        with ZipFile(
+            BytesIO(
+                self.session.get(f"/workspaces/{workspace_id}/certificates").content
+            )
+        ) as z:
+            with z.open('ca.cert') as f:
+                return f.read()
+
+    def get_auth_token_details(self) -> dict:
+        return self.session.get(f"/auth/token/details").json()
+
+    def get_workspace(self, workspace_id: Optional[str] = None) -> dict:
+        if not workspace_id:
+            workspace_id = self.default_workspace_id
+        return self.session.get(f"/workspaces/{workspace_id}").json()
+
+    def get_workspaces(self) -> list[dict]:
+        # TODO: This seems only return [] with Personal Access Tokens as of Sept 7 '23
+        return self.session.get("/workspaces").json()
+
+    def get_topics(self, workspace_id: Optional[str] = None) -> list[dict]:
+        if not workspace_id:
+            workspace_id = self.default_workspace_id
+        return self.session.get(f"/{workspace_id}/topics").json()
+
+    def post_topic(
+        self,
+        topic_name: str,
+        workspace_id: Optional[str] = None,
+        topic_partitions: int = 2,
+        topic_rep_factor: int = 2,
+        topic_ret_minutes: int = 10080,
+        topic_ret_bytes: int = 52428800,
+    ) -> dict:
+        if not workspace_id:
+            workspace_id = self.default_workspace_id
+        d = {
+            "name": topic_name,
+            "configuration": {
+                "partitions": topic_partitions,
+                "replicationFactor": topic_rep_factor,
+                "retentionInMinutes": topic_ret_minutes,
+                "retentionInBytes": topic_ret_bytes,
+            },
+        }
+        return self.session.post(f"/{workspace_id}/topics", json=d).json()
+
+    def get_topic(self, topic_name: str, workspace_id: Optional[str] = None) -> dict:
+        if not workspace_id:
+            workspace_id = self.default_workspace_id
+        return self.session.get(f"/{workspace_id}/topics/{topic_name}").json()
---
+++ src/PythonClient/src/quixstreams/dataframes/platforms/quix/config.py
@@ -0,0 +1,292 @@
+from .api import QuixPortalApiService
+from pathlib import Path
+from os import getcwd
+from requests import HTTPError
+from typing import Optional, Tuple
+from tempfile import gettempdir
+
+
+__all__ = ("QuixKafkaConfigsBuilder",)
+
+
+class QuixKafkaConfigsBuilder:
+    """
+     Retrieves all the necessary information from the Quix API and builds all the
+     objects required to connect a confluent-kafka client to the Quix Platform.
+
+     If not executed within the Quix platform directly, you must provide a Quix
+     "sdk-token" (aka workspace token) or Personal Access Token.
+
+     Ideally you also know your workspace name or id. If not, you can search for it
+     using a known topic name, but note the search space is limited to the access level
+     of your token.
+
+    If using Quix Platform, your most common use will be:
+
+     api = QuixPortalApiInstance()
+     builder = QuixKafkaConfigsBuilder(quix_portal_api_service=api)
+     topics = ['topic_a', 'topic_b']
+     cons_grp = 'my_consumer_group
+     cfgs, topics, cons_grp = builder.get_confluent_client_configs(topics, cons_grp)
+
+     Then, if using a Quix Runner:
+     runner = Runner(
+         broker_address=cfgs.pop('bootstrap_servers'),
+         consumer_group=cons_grp,
+         producer_extra_configs=cfgs,
+         consumer_extra_configs=cfgs
+     )
+    """
+
+    # TODO: Consider alterations to the current Topic class to accommodate the
+    # difference between topic name and id (hiding the workspace part from the user)
+
+    # TODO: Consider a workspace class?
+    def __init__(
+        self,
+        quix_portal_api_service: Optional[QuixPortalApiService] = None,
+        workspace_id: Optional[str] = None,
+        workspace_cert_path: Optional[str] = None,
+    ):
+        self.api = quix_portal_api_service or QuixPortalApiService()
+        if workspace_id:
+            self._workspace_id = workspace_id
+        else:
+            self._workspace_id = self.api.default_workspace_id
+        self._workspace_cert_path = workspace_cert_path
+        self._confluent_broker_config = None
+        self._quix_broker_config = None
+        self._quix_broker_settings = None
+        self._workspace_meta = None
+
+    class NoWorkspaceFound(Exception):
+        pass
+
+    class MultipleWorkspaces(Exception):
+        pass
+
+    class QuixApiKafkaAuthConfigMap:
+        names = {
+            "saslMechanism": "sasl.mechanisms",
+            "securityMode": "security.protocol",
+            "address": "bootstrap.servers",
+            "username": "sasl.username",
+            "password": "sasl.password",
+        }
+        values = {"ScramSha256": "SCRAM-SHA-256", "SaslSsl": "SASL_SSL"}
+
+    @property
+    def workspace_id(self) -> str:
+        if not self._workspace_id:
+            self.get_workspace_info()
+        return self._workspace_id
+
+    @property
+    def quix_broker_config(self) -> dict:
+        if not self._quix_broker_config:
+            self.get_workspace_info()
+        return self._quix_broker_config
+
+    @property
+    def quix_broker_settings(self) -> dict:
+        if not self._quix_broker_settings:
+            self.get_workspace_info()
+        return self._quix_broker_settings
+
+    @property
+    def confluent_broker_config(self) -> dict:
+        if not self._confluent_broker_config:
+            self.get_confluent_broker_config()
+        return self._confluent_broker_config
+
+    @property
+    def workspace_cert_path(self) -> str:
+        if not self._workspace_cert_path:
+            self._set_workspace_cert()
+        return self._workspace_cert_path
+
+    @property
+    def workspace_meta(self) -> dict:
+        if not self._workspace_meta:
+            self.get_workspace_info()
+        return self._workspace_meta
+
+    def append_workspace_id(self, s: str) -> str:
+        """
+        Add the workspace ID to a given string, typically a topic or consumer group id
+
+        :param s: the string to append to
+        :return: the string with workspace_id appended
+        """
+        return f"{self.workspace_id}-{s}" if not s.startswith(self.workspace_id) else s
+
+    def search_for_workspace(
+            self, workspace_name_or_id: Optional[str] = None
+    ) -> Optional[dict]:
+        # TODO: there is more to do here to accommodate the new "environments" in v2
+        # as it stands now, the search wont work with Quix v2 platform correctly if
+        # it's not a workspace_id
+        """
+        Search for a workspace given an expected workspace name or id.
+
+        :param workspace_name_or_id: the expected name or id of a workspace
+        :return: the workspace data dict if search success, else None
+        """
+        if not workspace_name_or_id:
+            workspace_name_or_id = self._workspace_id
+        try:
+            return self.api.get_workspace(workspace_id=workspace_name_or_id)
+        except HTTPError:
+            # check to see if they provided the workspace name instead
+            ws_list = self.api.get_workspaces()
+            for ws in ws_list:
+                if ws["name"] == workspace_name_or_id:
+                    return ws
+
+    def get_workspace_info(self, known_workspace_topic: Optional[str] = None):
+        """
+        Queries for workspace data from the Quix API, regardless of instance cache,
+        and updates instance attributes from query result.
+
+        :param known_workspace_topic: a topic you know to exist in some workspace
+        """
+        # TODO: more error handling with the wrong combo of ws_id and topic
+        if self._workspace_id:
+            ws_data = self.search_for_workspace(workspace_name_or_id=self._workspace_id)
+        else:
+            ws_data = self.search_for_topic_workspace(known_workspace_topic)
+        if not ws_data:
+            raise self.NoWorkspaceFound(
+                "No workspace was found for the given workspace/auth-token/topic combo"
+            )
+        self._workspace_id = ws_data.pop("workspaceId")
+        self._quix_broker_config = ws_data.pop("broker")
+        try:
+            self._quix_broker_settings = ws_data.pop("brokerSettings")
+        except KeyError:  # hold-over for platform v1
+            self._quix_broker_settings = {
+                "brokerType": ws_data['brokerType'],
+                "syncTopics": False
+            }
+        self._workspace_meta = ws_data
+
+    def search_workspace_for_topic(
+            self, workspace_id: str, topic: str
+    ) -> Optional[str]:
+        """
+        Search through all the topics in the given workspace id to see if there is a
+        match with the provided topic.
+
+        :param workspace_id: the workspace to search in
+        :param topic: the topic to search for
+        :return: the workspace_id if success, else None
+        """
+        topics = self.api.get_topics(workspace_id=workspace_id)
+        for t in topics:
+            if t["name"] == topic or t["id"] == topic:
+                return workspace_id
+
+    def search_for_topic_workspace(self, topic: str) -> Optional[dict]:
+        """
+        Find what workspace a topic belongs to.
+        If there is only one workspace altogether, it is assumed to be the workspace.
+        More than one means each workspace will be searched until the first hit.
+
+        :param topic: the topic to search for
+        :return: workspace data dict if topic search success, else None
+        """
+        ws_list = self.api.get_workspaces()
+        if len(ws_list) == 1:
+            return ws_list[0]
+        if topic is None:
+            raise self.MultipleWorkspaces(
+                "More than 1 workspace was found, so you must provide a topic name "
+                "to find the correct workspace"
+            )
+        for ws in ws_list:
+            if self.search_workspace_for_topic(ws["workspaceId"], topic):
+                return ws
+
+    def get_workspace_ssl_cert(self, extract_to_folder: Optional[Path] = None) -> str:
+        """
+        Gets and extracts zipped certificate from the API to provided folder.
+        If no path was provided, will dump to /tmp. Expects cert named 'ca.cert'.
+
+        :param extract_to_folder: path to folder to dump zipped cert file to
+        :return: full cert filepath as string
+        """
+        extract_to_folder = extract_to_folder or Path(gettempdir())
+        full_path = extract_to_folder / "ca.cert"
+        if not full_path.is_file():
+            extract_to_folder.mkdir(parents=True, exist_ok=True)
+            with open(full_path, 'wb') as f:
+                f.write(
+                    self.api.get_workspace_certificate(workspace_id=self._workspace_id)
+                )
+        return full_path.as_posix()
+
+    def _set_workspace_cert(self) -> str:
+        """
+        Will create a cert and assigns it to the workspace_cert_path property.
+        If there was no path provided at init, one is generated based on the cwd and
+        workspace_id.
+        Used by this class when generating configs (does some extra folder and class
+        config stuff that's not needed if you just want to get the cert only)
+
+        :return: full cert filepath as string
+        """
+        if self._workspace_cert_path:
+            folder = Path(self._workspace_cert_path)
+            if folder.name.endswith("cert"):
+                folder = folder.parent
+        else:
+            folder = Path(getcwd()) / "certificates" / self.workspace_id
+        self._workspace_cert_path = self.get_workspace_ssl_cert(
+            extract_to_folder=folder
+        )
+        return self._workspace_cert_path
+
+    def get_confluent_broker_config(self, known_topic: Optional[str] = None) -> dict:
+        """
+        Get the full client config dictionary required to authenticate a confluent-kafka
+        client to a Quix platform broker/workspace.
+
+        The returned config can be used directly by any confluent-kafka-python consumer/
+        producer (add your producer/consumer-specific configs afterward).
+
+        :param known_topic: a topic known to exist in some workspace
+        :return: a dict of confluent-kafka-python client settings (see librdkafka
+        config for more details)
+        """
+        self.get_workspace_info(known_workspace_topic=known_topic)
+        cfg_out = {}
+        for q_cfg, c_cfg in self.QuixApiKafkaAuthConfigMap.names.items():
+            value = self.quix_broker_config[q_cfg]
+            cfg_out[c_cfg] = self.QuixApiKafkaAuthConfigMap.values.get(value, value)
+        cfg_out["ssl.endpoint.identification.algorithm"] = "none"
+        cfg_out["ssl.ca.location"] = self._set_workspace_cert()
+        self._confluent_broker_config = cfg_out
+        return self._confluent_broker_config
+
+    def get_confluent_client_configs(
+        self, topics: list, consumer_group_id: Optional[str] = None
+    ) -> Tuple[dict, list[str], Optional[str]]:
+        """
+        Get all the values you need in order to use a confluent_kafka-based client
+        with a topic on a Quix platform broker/workspace.
+
+        The returned config can be used directly by any confluent-kafka-python consumer/
+        producer (add your producer/consumer-specific configs afterward).
+
+        The topics and consumer group are appended with any necessary values.
+
+        :param topics: list of topics
+        :param consumer_group_id: consumer group id, if needed
+        :return: a tuple with configs and altered versions of the topics
+        and consumer group name
+        """
+        return (
+            self.get_confluent_broker_config(topics[0]),
+            [self.append_workspace_id(t) for t in topics],
+            self.append_workspace_id(consumer_group_id) if consumer_group_id else None,
+        )
