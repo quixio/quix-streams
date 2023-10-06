b3c4a8815fff55036a77d40da7c22d2e2f1804c0
Daniil Gusev <daniil@quix.io>
2023-10-06 12:40:18 +0200

    Add compression type option for rocksdb
    Use "lz4" by default

--- src/StreamingDataFrames/streamingdataframes/state/rocksdb/options.py
+++ src/StreamingDataFrames/streamingdataframes/state/rocksdb/options.py
@@ -1,11 +1,24 @@
 import dataclasses
-from typing import Optional
+from typing import Optional, Mapping, Literal
 
 import rocksdict
+from rocksdict import DBCompressionType
 
 from .types import RocksDBOptionsProto
 
 __all__ = ("RocksDBOptions",)
+
+CompressionType = Literal["none", "snappy", "zlib", "bz2", "lz4", "lz4hc", "zstd"]
+
+COMPRESSION_TYPES: Mapping[CompressionType, DBCompressionType] = {
+    "none": DBCompressionType.none(),
+    "snappy": DBCompressionType.snappy(),
+    "zlib": DBCompressionType.zlib(),
+    "bz2": DBCompressionType.bz2(),
+    "lz4": DBCompressionType.lz4(),
+    "lz4hc": DBCompressionType.lz4hc(),
+    "zstd": DBCompressionType.zstd(),
+}
 
 
 @dataclasses.dataclass(frozen=True)
@@ -24,6 +37,7 @@ class RocksDBOptions(RocksDBOptionsProto):
     max_write_buffer_number: int = 3
     block_cache_size: int = 128 * 1024 * 1024  # 128MB
     enable_pipelined_write: bool = False
+    compression_type: CompressionType = "lz4"
     wal_dir: Optional[str] = None
     db_log_dir: Optional[str] = None
 
@@ -46,4 +60,7 @@ class RocksDBOptions(RocksDBOptionsProto):
         table_factory_options = rocksdict.BlockBasedOptions()
         table_factory_options.set_block_cache(rocksdict.Cache(self.block_cache_size))
         opts.set_block_based_table_factory(table_factory_options)
+
+        compression_type = COMPRESSION_TYPES[self.compression_type]
+        opts.set_compression_type(compression_type)
         return opts
