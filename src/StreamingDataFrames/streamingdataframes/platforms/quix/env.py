285439e542797759e16d806169d0ec96a5d502dc
Daniil Gusev <133032822+daniil-quix@users.noreply.github.com>
2023-10-30 16:16:17 +0100

    Validate state settings for Quix apps (#211)
    - Check if the "State Management" flag is enabled if the app is deployed on Quix
    - Check if the app's "state_dir" points to the correct location if deployed on Quix
    - Add QuixEnvironment singleton to access Quix-specific env vars

---
+++ src/StreamingDataFrames/streamingdataframes/platforms/quix/env.py
@@ -0,0 +1,73 @@
+import os
+from typing import Optional
+
+__all__ = ("QuixEnvironment", "QUIX_ENVIRONMENT")
+
+
+class QuixEnvironment:
+    """
+    Class to access various Quix platform environment settings
+    """
+
+    SDK_TOKEN = "Quix__Sdk__Token"
+    PORTAL_API = "Quix__Portal__Api"
+    WORKSPACE_ID = "Quix__Workspace__Id"
+    DEPLOYMENT_ID = "Quix__Deployment__Id"
+    STATE_MANAGEMENT_ENABLED = "Quix__Deployment__State__Enabled"
+
+    @property
+    def state_management_enabled(self) -> bool:
+        """
+        Check whether "State management" is enabled for the current deployment
+        :return: True if state management is enabled, otherwise False
+        """
+        return os.environ.get(self.STATE_MANAGEMENT_ENABLED, "") == "true"
+
+    @property
+    def deployment_id(self) -> Optional[str]:
+        """
+        Return current Quix deployment id.
+
+        This variable is meant to be set only by Quix Platform and only
+        when the application is deployed.
+
+        :return: deployment id or None
+        """
+        return os.environ.get(self.DEPLOYMENT_ID)
+
+    @property
+    def workspace_id(self) -> Optional[str]:
+        """
+        Return Quix workspace id if set
+        :return: workspace id or None
+        """
+        return os.environ.get(self.WORKSPACE_ID)
+
+    @property
+    def portal_api(self) -> Optional[str]:
+        """
+        Return Quix Portal API url if set
+
+        :return: portal API URL or None
+        """
+        return os.environ.get(self.PORTAL_API)
+
+    @property
+    def sdk_token(self) -> Optional[str]:
+        """
+        Return Quix SDK token if set
+        :return: sdk token or None
+        """
+        return os.environ.get(self.SDK_TOKEN)
+
+    @property
+    def state_dir(self) -> str:
+        """
+        Return application state directory on Quix.
+        :return: path to state dir
+        """
+        # TODO: Use env variables instead when they're available
+        return "/app/state"
+
+
+QUIX_ENVIRONMENT = QuixEnvironment()
