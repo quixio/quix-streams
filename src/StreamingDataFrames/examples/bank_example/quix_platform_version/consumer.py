7e45828b0228c2e4faa9d6030eed9caf0c0a793f
Daniil Gusev <daniil@quix.io>
2023-10-30 19:03:56 +0100

    Update examples

--- src/StreamingDataFrames/examples/bank_example/quix_platform_version/consumer.py
+++ src/StreamingDataFrames/examples/bank_example/quix_platform_version/consumer.py
@@ -1,8 +1,14 @@
-# It is suggested to use the `Application.Quix` when interacting with the Quix platform
+"""
+An application to process imaginary purchase transactions in real-time using Kafka
+running on Quix platform.
+
+In this application, we will simulate notifications for "Gold" accounts about
+purchase events larger than $1000
+"""
 
 from dotenv import load_dotenv
 
-from streamingdataframes import Application
+from streamingdataframes import Application, MessageContext
 from streamingdataframes.models.serializers import (
     QuixTimeseriesSerializer,
     QuixTimeseriesDeserializer,
@@ -12,34 +18,64 @@ from streamingdataframes.models.serializers import (
 # environment would be unnecessary there.
 load_dotenv("./bank_example/quix_platform_version/quix_vars.env")
 
-# Quix app has an option to auto create topics
-# Quix app does not require the broker being defined
+# Define your application and settings
+# Quix application is automatically configured to work with Quix platform
 app = Application.Quix(
-    "qts__purchase_notifier", auto_create_topics=True, auto_offset_reset="earliest"
+    "qts__purchase_notifier",
+    auto_offset_reset="earliest",
+    auto_create_topics=True,  # Quix app has an option to auto create topics
 )
+
+# Define an input topic with JSON deserializer
 input_topic = app.topic(
     "qts__purchase_events", value_deserializer=QuixTimeseriesDeserializer()
 )
+
+# Define an output topic with JSON dserializer
 output_topic = app.topic(
     "qts__user_notifications", value_serializer=QuixTimeseriesSerializer()
 )
 
-
-def uppercase_source(msg_value):
-    msg_value["transaction_source"] = msg_value["transaction_source"].upper()
-    return msg_value
-
-
-# "Gold" members get realtime notifications about purchase events larger than $1000
+# Create a StreamingDataFrame and start building your processing pipeline
 sdf = app.dataframe(topics_in=[input_topic])
+
+
+def uppercase_source(value: dict, ctx: MessageContext):
+    """
+    Upper-case field "transaction_source" for each processed message
+
+    :param value: message value, a dictionary with all deserialized message data
+    :param ctx: message context, it contains message metadata like key, topic, timestamp
+        etc.
+
+    :return: this function must either return None or a new dictionary
+    """
+    print(f'Processing message with key "{ctx.key}"')
+    value["transaction_source"] = value["transaction_source"].upper()
+    return value
+
+
+# Filter only messages with "account_class" == "Gold" and "transaction_amount" >= 1000
 sdf = sdf[
     (sdf["account_class"] == "Gold")
     & (sdf["transaction_amount"].apply(lambda x: abs(x)) >= 1000)
-]  # filter out any messages according to these conditionals
-sdf = sdf[["account_id", "transaction_amount", "transaction_source"]]  # column subset
-sdf = sdf.apply(uppercase_source)  # apply funcs must accept the message value as an arg
+]
+
+# Drop all fields except the ones we need
+sdf = sdf[["account_id", "transaction_amount", "transaction_source"]]
+
+# Transform field "transaction_source" to upper-case using a custom function
+sdf = sdf.apply(uppercase_source)
+
+# Add a new field with a notification text
 sdf["customer_notification"] = "A high cost purchase was attempted"  # add new column
-sdf = sdf.apply(lambda val: print(f"Sending update: {val}"))  # easy way to print out
+
+# Print the transformed message to the console
+sdf = sdf.apply(lambda val: print(f"Sending update: {val}"))
+
+# Send the message to the output topic
 sdf.to_topic(output_topic)
 
-app.run(sdf)
+if __name__ == "__main__":
+    # Start message processing
+    app.run(sdf)
