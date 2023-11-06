7ad247062d71b207e540de3bfc1624cca66e07f9
Tim Sawicki <136370015+tim-quix@users.noreply.github.com>
2023-11-06 10:10:40 -0500

    allow string param arguments for defining serializers (#221)
    Added ability to specify desired SERDES as string versions like "json", "str", etc.
    
    Changed default value SERDES for apps to JSON.

--- src/StreamingDataFrames/streamingdataframes/models/serializers/__init__.py
+++ src/StreamingDataFrames/streamingdataframes/models/serializers/__init__.py
@@ -1,4 +1,4 @@
-from .base import Serializer, Deserializer, SerializationContext
+from .base import *
 from .simple_types import (
     StringSerializer,
     StringDeserializer,
@@ -16,3 +16,27 @@ from .quix import (
     QuixTimeseriesSerializer,
     QuixDeserializer,
 )
+
+
+SERIALIZERS = {
+    "str": StringSerializer,
+    "string": StringSerializer,
+    "bytes": BytesSerializer,
+    "double": DoubleSerializer,
+    "int": IntegerSerializer,
+    "integer": IntegerSerializer,
+    "json": JSONSerializer,
+    "quix_timeseries": QuixTimeseriesSerializer,
+    "quix_events": QuixEventsSerializer,
+}
+
+DESERIALIZERS = {
+    "str": StringDeserializer,
+    "string": StringDeserializer,
+    "bytes": BytesDeserializer,
+    "double": DoubleDeserializer,
+    "int": IntegerDeserializer,
+    "integer": IntegerDeserializer,
+    "json": JSONDeserializer,
+    "quix": QuixDeserializer,
+}
