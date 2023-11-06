d7a4bb3a2e210a28c9b4186d93da20d9b1706ffa
Daniil Gusev <daniil@quix.io>
2023-11-06 20:49:02 +0100

    Add stateful funcs to examples

--- src/StreamingDataFrames/examples/bank_example/quix_platform_version/consumer.py
+++ src/StreamingDataFrames/examples/bank_example/quix_platform_version/consumer.py
@@ -8,34 +8,11 @@ purchase events larger than $1000
 
 from dotenv import load_dotenv
 
-from quixstreams import Application, MessageContext
-from quixstreams.models.serializers import (
-    QuixTimeseriesSerializer,
-    QuixDeserializer,
-)
+from quixstreams import Application, MessageContext, State
 
 # Reminder: the platform will have these values available by default so loading the
 # environment would be unnecessary there.
 load_dotenv("./bank_example/quix_platform_version/quix_vars.env")
-
-# Define your application and settings
-# Quix application is automatically configured to work with Quix platform
-app = Application.Quix(
-    "qts__purchase_notifier",
-    auto_offset_reset="earliest",
-    auto_create_topics=True,  # Quix app has an option to auto create topics
-)
-
-# Define an input topic with JSON deserializer
-input_topic = app.topic("qts__purchase_events", value_deserializer=QuixDeserializer())
-
-# Define an output topic with JSON dserializer
-output_topic = app.topic(
-    "qts__user_notifications", value_serializer=QuixTimeseriesSerializer()
-)
-
-# Create a StreamingDataFrame and start building your processing pipeline
-sdf = app.dataframe(input_topic)
 
 
 def uppercase_source(value: dict, ctx: MessageContext):
@@ -53,6 +30,38 @@ def uppercase_source(value: dict, ctx: MessageContext):
     return value
 
 
+def count_transactions(value: dict, ctx: MessageContext, state: State):
+    """
+    Track the number of transactions using persistent state
+
+    :param value: message value
+    :param ctx: message context with key, timestamp and other Kafka message metadata
+    :param state: instance of State store
+    """
+    total = state.get("total_transactions", 0)
+    total += 1
+    state.set("total_transactions", total)
+    value["total_transactions"] = total
+
+
+# Define your application and settings
+# Quix application is automatically configured to work with Quix platform
+app = Application.Quix(
+    "qts__purchase_notifier",
+    auto_offset_reset="earliest",
+    auto_create_topics=True,  # Quix app has an option to auto create topics
+)
+
+# Define an input topic with Quix deserializer
+input_topic = app.topic("qts__purchase_events", value_deserializer="quix")
+
+# Define an output topic with Quix Timeseries serializer
+output_topic = app.topic("qts__user_notifications", value_serializer="quix_timeseries")
+
+# Create a StreamingDataFrame and start building your processing pipeline
+sdf = app.dataframe(input_topic)
+
+
 # Filter only messages with "account_class" == "Gold" and "transaction_amount" >= 1000
 sdf = sdf[
     (sdf["account_class"] == "Gold")
@@ -62,11 +71,14 @@ sdf = sdf[
 # Drop all fields except the ones we need
 sdf = sdf[["account_id", "transaction_amount", "transaction_source"]]
 
+# Update the total number of transactions in state
+sdf = sdf.apply(count_transactions, stateful=True)
+
 # Transform field "transaction_source" to upper-case using a custom function
 sdf = sdf.apply(uppercase_source)
 
 # Add a new field with a notification text
-sdf["customer_notification"] = "A high cost purchase was attempted"  # add new column
+sdf["customer_notification"] = "A high cost purchase was attempted"
 
 # Print the transformed message to the console
 sdf = sdf.apply(lambda val, ctx: print(f"Sending update: {val}"))
