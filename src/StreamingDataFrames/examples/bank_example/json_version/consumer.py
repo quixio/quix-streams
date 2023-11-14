4c936b85a34506ed6843711b018f09200dd4029c
Daniil Gusev <daniil@quix.io>
2023-11-14 20:37:16 +0100

    Update examples

--- src/StreamingDataFrames/examples/bank_example/json_version/consumer.py
+++ src/StreamingDataFrames/examples/bank_example/json_version/consumer.py
@@ -9,36 +9,34 @@ from os import environ
 
 from dotenv import load_dotenv
 
-from quixstreams import Application, MessageContext, State
+from quixstreams import Application, State, message_key
 
 load_dotenv("./env_vars.env")
 
 
-def count_transactions(value: dict, ctx: MessageContext, state: State):
+def count_transactions(value: dict, state: State):
     """
     Track the number of transactions using persistent state
 
     :param value: message value
-    :param ctx: message context with key, timestamp and other Kafka message metadata
     :param state: instance of State store
     """
     total = state.get("total_transactions", 0)
     total += 1
     state.set("total_transactions", total)
-    value["total_transactions"] = total
+    return total
 
 
-def uppercase_source(value: dict, ctx: MessageContext):
+def uppercase_source(value: dict):
     """
     Upper-case field "transaction_source" for each processed message
 
     :param value: message value, a dictionary with all deserialized message data
-    :param ctx: message context, it contains message metadata like key, topic, timestamp
-        etc.
+
 
     :return: this function must either return None or a new dictionary
     """
-    print(f'Processing message with key "{ctx.key}"')
+    print(f'Processing message with key "{message_key()}"')
     value["transaction_source"] = value["transaction_source"].upper()
     return value
 
@@ -62,28 +60,25 @@ output_topic = app.topic("json__user_notifications", value_serializer="json")
 sdf = app.dataframe(input_topic)
 
 # Filter only messages with "account_class" == "Gold" and "transaction_amount" >= 1000
-sdf = sdf[
-    (sdf["account_class"] == "Gold")
-    & (sdf["transaction_amount"].apply(lambda x, ctx: abs(x)) >= 1000)
-]
+sdf = sdf[(sdf["account_class"] == "Gold") & (sdf["transaction_amount"].abs() >= 1000)]
 
 # Drop all fields except the ones we need
 sdf = sdf[["account_id", "transaction_amount", "transaction_source"]]
 
-# Update the total number of transactions in state
-sdf = sdf.apply(count_transactions, stateful=True)
+# Update the total number of transactions in state and save result to the message
+sdf["total_transactions"] = sdf.apply(count_transactions, stateful=True)
 
 # Transform field "transaction_source" to upper-case using a custom function
-sdf = sdf.apply(uppercase_source)
+sdf["transaction_source"] = sdf["transaction_source"].apply(lambda v: v.upper())
 
 # Add a new field with a notification text
 sdf["customer_notification"] = "A high cost purchase was attempted"
 
 # Print the transformed message to the console
-sdf = sdf.apply(lambda val, ctx: print(f"Sending update: {val}"))
+sdf = sdf.update(lambda val: print(f"Sending update: {val}"))
 
 # Send the message to the output topic
-sdf.to_topic(output_topic)
+sdf = sdf.to_topic(output_topic)
 
 if __name__ == "__main__":
     # Start message processing
