6c8aa9c7ef0e2af34a9e8bd6643509b6e1d5aeb0
Haris Botic <haris@quix.io>
2024-03-01 11:34:57 +0100

    remote comments

--- quixstreams/models/topics/topic.py
+++ quixstreams/models/topics/topic.py
@@ -227,9 +227,6 @@ class Topic:
             # should be processed as a separate message
             rows = []
             for item in value:
-                # if not isinstance(item, dict):
-                #     raise TypeError(f'Row value must be a dict, but got "{type(item)}"')
-
                 rows.append(
                     Row(
                         value=item,
@@ -239,9 +236,6 @@ class Topic:
                     )
                 )
             return rows
-
-        # if not isinstance(value, dict):
-        #     raise TypeError(f'Row value must be a dict, but got "{type(value)}"')
 
         return Row(
             value=value,
