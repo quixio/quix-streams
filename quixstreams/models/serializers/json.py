ed69ab173b47f71081d789c335dac737f7438644
Remy Gwaramadze <remy@quix.io>
2024-08-15 10:34:34 +0200

    Integrate Schema Registry with JSON Schema

--- quixstreams/models/serializers/json.py
+++ quixstreams/models/serializers/json.py
@@ -1,5 +1,12 @@
+import json
 from typing import Callable, Union, Mapping, Optional, Any, Iterable
 
+from confluent_kafka.schema_registry import SchemaRegistryClient, SchemaRegistryError
+from confluent_kafka.schema_registry.json_schema import (
+    JSONDeserializer as _JSONDeserializer,
+    JSONSerializer as _JSONSerializer,
+)
+from confluent_kafka.serialization import SerializationError as _SerializationError
 from jsonschema import ValidationError, Draft202012Validator
 from jsonschema.protocols import Validator
 
@@ -9,6 +16,10 @@ from quixstreams.utils.json import (
 )
 from .base import Serializer, Deserializer, SerializationContext
 from .exceptions import SerializationError
+from .schema_registry import (
+    SchemaRegistryClientConfig,
+    SchemaRegistrySerializationConfig,
+)
 
 __all__ = ("JSONSerializer", "JSONDeserializer")
 
@@ -19,6 +30,10 @@ class JSONSerializer(Serializer):
         dumps: Callable[[Any], Union[str, bytes]] = default_dumps,
         schema: Optional[Mapping] = None,
         validator: Optional[Validator] = None,
+        schema_registry_client_config: Optional[SchemaRegistryClientConfig] = None,
+        schema_registry_serialization_config: Optional[
+            SchemaRegistrySerializationConfig
+        ] = None,
     ):
         """
         Serializer that returns data in json format.
@@ -28,6 +43,11 @@ class JSONSerializer(Serializer):
             Default - `None`
         :param validator: A jsonschema validator used to validate the data. Takes precedences over the schema.
             Default - `None`
+        :param schema_registry_client_config: If provided, serialization is offloaded to Confluent's JSONSerializer.
+            Default - `None`
+        :param schema_registry_serialization_config: Additional configuration for Confluent's JSONSerializer.
+            Default - `None`
+            >***NOTE:*** `schema_registry_client_config` must also be set.
         """
 
         if schema and not validator:
@@ -40,7 +60,35 @@ class JSONSerializer(Serializer):
         if self._validator:
             self._validator.check_schema(self._validator.schema)
 
+        self._schema_registry_serializer = None
+        if schema_registry_client_config:
+            if not schema:
+                raise ValueError(
+                    "If `schema_registry_serialization_config` is provided, "
+                    "`schema` must also be set."
+                )
+
+            client_config = schema_registry_client_config.as_dict(
+                plaintext_secrets=True,
+            )
+
+            serialization_config = {}
+            if schema_registry_serialization_config:
+                serialization_config = schema_registry_serialization_config.as_dict()
+
+            self._schema_registry_serializer = _JSONSerializer(
+                schema_str=json.dumps(schema),
+                schema_registry_client=SchemaRegistryClient(client_config),
+                conf=serialization_config,
+            )
+
     def __call__(self, value: Any, ctx: SerializationContext) -> Union[str, bytes]:
+        if self._schema_registry_serializer is not None:
+            try:
+                return self._schema_registry_serializer(value, ctx)
+            except (SchemaRegistryError, _SerializationError) as exc:
+                raise SerializationError(str(exc)) from exc
+
         return self._to_json(value)
 
     def _to_json(self, value: Any):
@@ -62,6 +110,7 @@ class JSONDeserializer(Deserializer):
         loads: Callable[[Union[bytes, bytearray]], Any] = default_loads,
         schema: Optional[Mapping] = None,
         validator: Optional[Validator] = None,
+        schema_registry_client_config: Optional[SchemaRegistryClientConfig] = None,
     ):
         """
         Deserializer that parses data from JSON
@@ -71,6 +120,8 @@ class JSONDeserializer(Deserializer):
         :param schema: A schema used to validate the data using [`jsonschema.Draft202012Validator`](https://python-jsonschema.readthedocs.io/en/stable/api/jsonschema/validators/#jsonschema.validators.Draft202012Validator).
             Default - `None`
         :param validator: A jsonschema validator used to validate the data. Takes precedences over the schema.
+            Default - `None`
+        :param schema_registry_client_config: If provided, deserialization is offloaded to Confluent's JSONDeserializer.
             Default - `None`
         """
 
@@ -84,9 +135,31 @@ class JSONDeserializer(Deserializer):
         if self._validator:
             self._validator.check_schema(self._validator.schema)
 
+        self._schema_registry_deserializer = None
+        if schema_registry_client_config:
+            if not schema:
+                raise ValueError(
+                    "If `schema_registry_serialization_config` is provided, "
+                    "`schema` must also be set."
+                )
+
+            client_config = schema_registry_client_config.as_dict(
+                plaintext_secrets=True,
+            )
+            self._schema_registry_deserializer = _JSONDeserializer(
+                schema_str=json.dumps(schema),
+                schema_registry_client=SchemaRegistryClient(client_config),
+            )
+
     def __call__(
         self, value: bytes, ctx: SerializationContext
     ) -> Union[Iterable[Mapping], Mapping]:
+        if self._schema_registry_deserializer is not None:
+            try:
+                return self._schema_registry_deserializer(value, ctx)
+            except (SchemaRegistryError, _SerializationError) as exc:
+                raise SerializationError(str(exc)) from exc
+
         try:
             data = self._loads(value)
         except (ValueError, TypeError) as exc:
