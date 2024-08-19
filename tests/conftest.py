81c62ac2c7e7a74a55a46071c384da8fef6b9ef5
Remy Gwaramadze <gwaramadze@users.noreply.github.com>
2024-08-19 15:39:04 +0200

    Schema Registry 2: Test Container  (#449)
    * Fix type hinting in kafka_container fixture
    
    * Rename kafka_container variable in kafka_container function
    
    Prevent potential variable shadowing
    
    * Create wait_for_container_readiness function
    
    Extracted the logic from ContainerHelper.start_kafka_container as it
    will be useful for other containers as well.
    
    * Create a dedicated network for Kafka test container
    
    Current setup supports calling Kafka container from host machine only,
    from pytest process specifically. This change is required to connect
    services in other containers (eg. Schema Registry) as long as they
    share the same network.
    
    * Create schema_registry_container fixture
    
    * Create a compatibility layer to support testing in Python 3.8
    
    The changes in this commit can be removed once we discontinue
    support for Python 3.8.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,10 @@
 import logging.config
 from collections import namedtuple
+from typing import Generator
 
 import pytest
 
+from .compat import Network
 from .containerhelper import ContainerHelper
 from .logging import LOGGING_CONFIG, patch_logger_class
 
@@ -17,7 +19,14 @@ pytest_plugins = [
     "tests.test_quixstreams.test_state.test_rocksdb.test_windowed.fixtures",
 ]
 
-KafkaContainer = namedtuple("KafkaContainer", ("broker_address",))
+KafkaContainer = namedtuple(
+    "KafkaContainer",
+    ["broker_address", "internal_broker_address"],
+)
+SchemaRegistryContainer = namedtuple(
+    "SchemaRegistryContainer",
+    ["schema_registry_address"],
+)
 
 test_logger = logging.getLogger("quixstreams.tests")
 
@@ -34,14 +43,41 @@ def log_test_progress(request: pytest.FixtureRequest):
 
 
 @pytest.fixture(scope="session")
-def kafka_container() -> KafkaContainer:
+def network():
+    with Network() as network:
+        yield network
+
+
+@pytest.fixture(scope="session")
+def kafka_container(network: Network) -> Generator[KafkaContainer, None, None]:
     (
-        kafka_container,
-        broker_address,
-        kafka_port,
+        container,
+        internal_broker_address,
+        external_broker_address,
     ) = ContainerHelper.create_kafka_container()
-    test_logger.debug(f"Starting Kafka container on {broker_address}")
-    ContainerHelper.start_kafka_container(kafka_container)
-    test_logger.debug(f"Started Kafka container on {broker_address}")
-    yield KafkaContainer(broker_address=broker_address)
-    kafka_container.stop()
+    test_logger.debug(f"Starting Kafka container on {external_broker_address}")
+    ContainerHelper.start_kafka_container(container, network)
+    test_logger.debug(f"Started Kafka container on {external_broker_address}")
+    yield KafkaContainer(
+        broker_address=external_broker_address,
+        internal_broker_address=internal_broker_address,
+    )
+    container.stop()
+
+
+@pytest.fixture(scope="session")
+def schema_registry_container(
+    network: Network, kafka_container: KafkaContainer
+) -> Generator[SchemaRegistryContainer, None, None]:
+    container, schema_registry_address = (
+        ContainerHelper.create_schema_registry_container(
+            kafka_container.internal_broker_address
+        )
+    )
+    test_logger.debug(
+        f"Starting Schema Registry container on {schema_registry_address}"
+    )
+    ContainerHelper.start_schema_registry_container(container, network)
+    test_logger.debug(f"Started Schema Registry container on {schema_registry_address}")
+    yield SchemaRegistryContainer(schema_registry_address=schema_registry_address)
+    container.stop()
