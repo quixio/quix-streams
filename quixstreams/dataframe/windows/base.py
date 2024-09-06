ff6c1acc7d89b9010220653e92cc110165820f91
Remy Gwaramadze <remy@quix.io>
2024-09-06 11:20:31 +0200

    Optimize get_window_ranges function
    deque.appendleft is roughly 10x faster than list.insert(0, ...)

--- quixstreams/dataframe/windows/base.py
+++ quixstreams/dataframe/windows/base.py
@@ -1,4 +1,5 @@
-from typing import Any, Optional, Callable, Tuple, List
+from collections import deque
+from typing import Any, Deque, Optional, Callable, Tuple
 
 from typing_extensions import TypedDict
 
@@ -17,7 +18,7 @@ WindowMergeFunc = Callable[[Any], Any]
 
 def get_window_ranges(
     timestamp_ms: int, duration_ms: int, step_ms: Optional[int] = None
-) -> List[Tuple[int, int]]:
+) -> Deque[Tuple[int, int]]:
     """
     Get a list of window ranges for the given timestamp.
     :param timestamp_ms: timestamp in milliseconds
@@ -28,14 +29,14 @@ def get_window_ranges(
     if not step_ms:
         step_ms = duration_ms
 
-    window_ranges = []
+    window_ranges = deque()
     current_window_start = timestamp_ms - (timestamp_ms % step_ms)
 
     while (
         current_window_start > timestamp_ms - duration_ms and current_window_start >= 0
     ):
         window_end = current_window_start + duration_ms
-        window_ranges.insert(0, (current_window_start, window_end))
+        window_ranges.appendleft((current_window_start, window_end))
         current_window_start -= step_ms
 
     return window_ranges
