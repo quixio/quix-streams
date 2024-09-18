2724ada7b90e46dbc3da04ed781398ed91cd440f
Remy Gwaramadze <remy@quix.io>
2024-09-18 10:47:22 +0200

    Create store and transaction_state fixtures for TestWindowedRocksDBPartitionTransactionState
    Moved some common parts to fixtures

--- tests/test_quixstreams/test_state/test_rocksdb/test_windowed/test_state.py
+++ tests/test_quixstreams/test_state/test_rocksdb/test_windowed/test_state.py
@@ -1,28 +1,40 @@
-class TestWindowedRocksDBPartitionTransactionState:
-    def test_update_window(self, windowed_rocksdb_store_factory):
-        store = windowed_rocksdb_store_factory()
-        store.assign_partition(0)
-        prefix = b"__key__"
+from contextlib import contextmanager
+
+import pytest
+
+
+@pytest.fixture
+def store(windowed_rocksdb_store_factory):
+    store = windowed_rocksdb_store_factory()
+    store.assign_partition(0)
+    return store
+
+
+@pytest.fixture
+def transaction_state(store):
+    @contextmanager
+    def _transaction_state():
         with store.start_partition_transaction(0) as tx:
-            state = tx.as_state(prefix=prefix)
+            yield tx.as_state(prefix=b"__key__")
+
+    return _transaction_state
+
+
+class TestWindowedRocksDBPartitionTransactionState:
+    def test_update_window(self, transaction_state):
+        with transaction_state() as state:
             state.update_window(start_ms=0, end_ms=10, value=1, timestamp_ms=2)
             assert state.get_window(start_ms=0, end_ms=10) == 1
 
-        with store.start_partition_transaction(0) as tx:
-            state = tx.as_state(prefix=prefix)
+        with transaction_state() as state:
             assert state.get_window(start_ms=0, end_ms=10) == 1
 
-    def test_expire_windows(self, windowed_rocksdb_store_factory):
-        store = windowed_rocksdb_store_factory()
-        store.assign_partition(0)
-        prefix = b"__key__"
-        with store.start_partition_transaction(0) as tx:
-            state = tx.as_state(prefix=prefix)
+    def test_expire_windows(self, transaction_state):
+        with transaction_state() as state:
             state.update_window(start_ms=0, end_ms=10, value=1, timestamp_ms=2)
             state.update_window(start_ms=10, end_ms=20, value=2, timestamp_ms=10)
 
-        with store.start_partition_transaction(0) as tx:
-            state = tx.as_state(prefix=prefix)
+        with transaction_state() as state:
             state.update_window(start_ms=20, end_ms=30, value=3, timestamp_ms=20)
             expired = state.expire_windows(duration_ms=10)
             # "expire_windows" must update the expiration index so that the same
@@ -35,8 +47,7 @@ class TestWindowedRocksDBPartitionTransactionState:
             ((10, 20), 2),
         ]
 
-        with store.start_partition_transaction(0) as tx:
-            state = tx.as_state(prefix=prefix)
+        with transaction_state() as state:
             assert state.get_window(start_ms=0, end_ms=10) is None
             assert state.get_window(start_ms=10, end_ms=20) is None
             assert state.get_window(start_ms=20, end_ms=30) == 3
