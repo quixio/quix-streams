a4cac55c376acc1f28b160814bfd000381a93135
Tim Sawicki <tim@quix.io>
2024-09-23 13:35:45 -0400

    remove unneeded change in app

--- quixstreams/app.py
+++ quixstreams/app.py
@@ -745,7 +745,6 @@ class Application:
                 DeprecationWarning,
             )
         self._setup_signal_handlers()
-        dataframes_composed = self._dataframe_registry.compose_all()
 
         logger.info(
             f"Starting the Application with the config: "
@@ -783,6 +782,8 @@ class Application:
 
             # Initialize the checkpoint
             self._processing_context.init_checkpoint()
+
+            dataframes_composed = self._dataframe_registry.compose_all()
 
             while self._running:
                 if self._state_manager.recovery_required:
