978b9953f1a87a8fa4fea7246f417ffde4eedac0
Remy Gwaramadze <remy@quix.io>
2024-09-24 16:13:55 +0200

    Decouple state operations from aggregate functions

--- quixstreams/dataframe/windows/time_based.py
+++ quixstreams/dataframe/windows/time_based.py
@@ -36,6 +36,7 @@ class FixedTimeWindow:
         grace_ms: int,
         name: str,
         aggregate_func: WindowAggregateFunc,
+        aggregate_default: Any,
         dataframe: "StreamingDataFrame",
         merge_func: Optional[WindowMergeFunc] = None,
         step_ms: Optional[int] = None,
@@ -47,6 +48,7 @@ class FixedTimeWindow:
         self._grace_ms = grace_ms
         self._name = name
         self._aggregate_func = aggregate_func
+        self._aggregate_default = aggregate_default
         self._merge_func = merge_func or _default_merge_func
         self._dataframe = dataframe
         self._step_ms = step_ms
@@ -63,6 +65,7 @@ class FixedTimeWindow:
     ) -> Tuple[List[WindowResult], List[WindowResult]]:
         duration_ms = self._duration_ms
         grace_ms = self._grace_ms
+        default = self._aggregate_default
 
         latest_timestamp = state.get_latest_timestamp()
         ranges = get_window_ranges(
@@ -86,7 +89,9 @@ class FixedTimeWindow:
                 )
                 continue
 
-            aggregated = self._aggregate_func(start, end, timestamp_ms, value, state)
+            current_value = state.get_window(start, end, default=default)
+            aggregated = self._aggregate_func(current_value, value)
+            state.update_window(start, end, timestamp_ms=timestamp_ms, value=aggregated)
             updated_windows.append(
                 {
                     "start": start,
