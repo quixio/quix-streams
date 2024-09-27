5b57ee1998d4d905e7eeb43217ec277b5414191d
Daniil Gusev <daniil@quix.io>
2024-09-27 12:52:18 +0200

    Expose existing sinks for backwards compatibility

--- quixstreams/sinks/__init__.py
+++ quixstreams/sinks/__init__.py
@@ -1,4 +1,6 @@
 from .base import BatchingSink, SinkBatch, BaseSink, SinkBackpressureError, SinkManager
+from .core.csv import CSVSink
+from .core.influxdb3 import InfluxDB3Sink
 
 __all__ = [
     "BaseSink",
@@ -6,4 +8,6 @@ __all__ = [
     "SinkBackpressureError",
     "SinkBatch",
     "SinkManager",
+    "CSVSink",
+    "InfluxDB3Sink",
 ]
