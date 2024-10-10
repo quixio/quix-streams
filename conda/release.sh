02093887ff797b178bf45c457434a464e82ff54c
Remy Gwaramadze <gwaramadze@users.noreply.github.com>
2024-10-10 15:23:47 +0200

    Add Conda release script (#571)

---
+++ conda/release.sh
@@ -0,0 +1,8 @@
+QUIXSTREAMS_VERSION=$(grep '__version__' ./quixstreams/__init__.py | cut -d '"' -f 2)
+
+docker run --rm \
+-v ./conda:/conda \
+-w /conda \
+-e ANACONDA_API_TOKEN=$ANACONDA_API_TOKEN \
+-e QUIXSTREAMS_VERSION=$QUIXSTREAMS_VERSION \
+continuumio/miniconda3 bash -c 'conda install --yes conda-build anaconda-client && conda build --channel conda-forge --user quixio .'
