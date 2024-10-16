ab14da4f22c63efbc92053175b308fed39d14841
Tim Sawicki <tim@quix.io>
2024-10-16 17:39:58 -0400

    changes to add a little bit more abstraction to the sink

--- quixstreams/sinks/community/iceberg.py
+++ quixstreams/sinks/community/iceberg.py
@@ -1,9 +1,12 @@
+from dataclasses import dataclass
 import logging
 import random
 import time
 from datetime import datetime
 from io import BytesIO
-from typing import Optional
+from typing import Optional, Literal, get_args
+from importlib import import_module
+
 
 from quixstreams.sinks import SinkBatch, BatchingSink, SinkBackpressureError
 
@@ -11,13 +14,6 @@ try:
     import pyarrow as pa
     import pyarrow.parquet as pq
     from pyiceberg.transforms import DayTransform, IdentityTransform
-    from pyiceberg.catalog.glue import (
-        GlueCatalog,
-        AWS_REGION,
-        AWS_ACCESS_KEY_ID,
-        AWS_SECRET_ACCESS_KEY,
-        AWS_SESSION_TOKEN,
-    )
     from pyiceberg.partitioning import PartitionSpec, PartitionField
     from pyiceberg.schema import Schema, NestedField
     from pyiceberg.types import StringType, TimestampType
@@ -25,62 +21,116 @@ try:
 except ImportError as exc:
     raise ImportError(
         f"Package {exc.name} is missing: "
-        "run pip install quixstreams[aws_iceberg] to use this sink"
+        f"do 'pip install quixstreams[iceberg]' to use this sink"
     ) from exc
 
 
 logger = logging.getLogger(__name__)
 
+DataCatalogSpec = Literal["aws_glue"]
 
-class IcebergSink(BatchingSink):
-    """
-    IcebergSink is a sink that writes batches of data to an Apache Iceberg table
-    stored in AWS S3 using the AWS Glue Data Catalog.
 
-    It serializes incoming data batches into Parquet format and appends them to the
-    Iceberg table, updating the table schema as necessary.
-    """
+@dataclass
+class IcebergSinkConfig:
+    location: str
+    auth: dict
 
+
+class IcebergAWSConfig(IcebergSinkConfig):
     def __init__(
         self,
-        table_name: str,
         aws_s3_uri: str,
         aws_region: Optional[str] = None,
         aws_access_key_id: Optional[str] = None,
         aws_secret_access_key: Optional[str] = None,
         aws_session_token: Optional[str] = None,
-        schema: Optional[Schema] = None,
-        partition_spec: Optional[PartitionSpec] = None,
     ):
         """
-        Initializes the S3Sink with the specified configuration.
-
-
-        :param table_name: The name of the Iceberg table.
         :param aws_s3_uri: The S3 URI where the table data will be stored
             (e.g., 's3://your-bucket/warehouse/').
         :param aws_region: The AWS region for the S3 bucket and Glue catalog.
         :param aws_access_key_id: the AWS access key ID.
-            NOTE: can alternatively set the AWS_ACCESS_KEY_ID environment variable.
+            NOTE: can alternatively set the AWS_ACCESS_KEY_ID environment variable
+            when using AWS Glue.
         :param aws_secret_access_key: the AWS secret access key.
-            NOTE: can alternatively set the AWS_SECRET_ACCESS_KEY environment variable.
+            NOTE: can alternatively set the AWS_SECRET_ACCESS_KEY environment variable
+            when using AWS Glue.
         :param aws_session_token: a session token (or will be generated for you).
-            NOTE: can alternatively set the AWS_SESSION_TOKEN environment variable.
+            NOTE: can alternatively set the AWS_SESSION_TOKEN environment variable when
+            using AWS Glue.
+        """
+        self.location = aws_s3_uri
+        self.auth = {
+            "client.region": aws_region,
+            "client.access-key-id": aws_access_key_id,
+            "client.secret-access-key": aws_secret_access_key,
+            "client.session-token": aws_session_token,
+        }
+
+
+class IcebergSink(BatchingSink):
+    """
+    IcebergSink writes batches of data to an Apache Iceberg table.
+
+    The data will by default include the kafka message key, value, and timestamp.
+
+    It serializes incoming data batches into Parquet format and appends them to the
+    Iceberg table, updating the table schema as necessary.
+
+    Currently supports Apache Iceberg hosted in:
+    - AWS
+
+    Currently supports data catalogs:
+    - AWS Glue
+    """
+
+    def __init__(
+        self,
+        table_name: str,
+        config: IcebergAWSConfig,
+        data_catalog_spec: DataCatalogSpec,
+        schema: Optional[Schema] = None,
+        partition_spec: Optional[PartitionSpec] = None,
+    ):
+        """
+        :param table_name: The name of the Iceberg table.
+        :param config: An IcebergConfig with all the various connection parameters.
+        :param data_catalog_spec: data cataloger to use (ex. for AWS Glue, "aws_glue").
         :param schema: The Iceberg table schema. If None, a default schema is used.
         :param partition_spec: The partition specification for the table.
             If None, a default is used.
-        """
-        super().__init__()
 
-        # Configure Iceberg Catalog using AWS Glue.
-        self.catalog = GlueCatalog(
-            name="glue_catalog",
-            **{
-                AWS_REGION: aws_region,
-                AWS_ACCESS_KEY_ID: aws_access_key_id,
-                AWS_SECRET_ACCESS_KEY: aws_secret_access_key,
-                AWS_SESSION_TOKEN: aws_session_token,
-            },
+        Example setup using an AWS-hosted Iceberg with AWS Glue:
+
+        from quixstreams.sinks.community.iceberg import IcebergSink, IcebergAWSConfig
+
+        iceberg_config = IcebergAWSConfig(
+            aws_s3_uri="", aws_region="", aws_access_key_id="", aws_secret_access_key=""
+        )
+
+        iceberg_sink = IcebergSink(
+            table_name="glue.sink-test",
+            config=iceberg_config,
+            data_catalog_spec="aws_glue",
+        )
+
+        app = Application(broker_address='localhost:9092', auto_offset_reset="earliest")
+        topic = app.topic('sink_topic')
+        sdf = app.dataframe(topic=topic).print(metadata=True)
+        sdf.sink(iceberg_sink)
+
+        if __name__ == "__main__":
+            app.run()
+        """
+
+        super().__init__()
+        self.iceberg_config = config
+
+        # Configure Iceberg Catalog
+        data_catalog_cls = _import_data_catalog(data_catalog_spec)
+        self.data_catalog = data_catalog_cls(
+            name=f"{data_catalog_spec}_catalog",
+            **self.iceberg_config.auth,
         )
 
         # Set up the schema.
@@ -125,14 +175,16 @@ class IcebergSink(BatchingSink):
             partition_spec = PartitionSpec(fields=partition_fields)
 
             # Create the Iceberg table if it doesn't exist.
-            self.table = self.catalog.create_table_if_not_exists(
+            self.table = self.data_catalog.create_table_if_not_exists(
                 identifier=table_name,
                 schema=schema,
-                location=aws_s3_uri,
+                location=self.iceberg_config.location,
                 partition_spec=partition_spec,
                 properties={"write.distribution-mode": "fanout"},
             )
-            logger.info(f"Loaded Iceberg table '{table_name}' at '{aws_s3_uri}'.")
+            logger.info(
+                f"Loaded Iceberg table '{table_name}' at '{self.iceberg_config.location}'."
+            )
 
     def write(self, batch: SinkBatch):
         """
@@ -150,7 +202,7 @@ class IcebergSink(BatchingSink):
             parquet_table = pq.read_table(input_buffer)
 
             # Reload the table to get the latest metadata
-            self.table = self.catalog.load_table(self.table.name())
+            self.table = self.data_catalog.load_table(self.table.name())
 
             # Update the table schema if necessary.
             with self.table.update_schema() as update:
@@ -210,3 +262,24 @@ def _serialize_batch_values(batch: SinkBatch) -> bytes:
     with BytesIO() as f:
         pq.write_table(table, f, compression="snappy")
         return f.getvalue()
+
+
+def _import_data_catalog(data_catalog_spec):
+    """
+    A way to dynamically load data catalogs which may require other imports
+    """
+    if data_catalog_spec not in get_args(DataCatalogSpec):
+        raise ValueError(f"Unsupported data_catalog_spec: {data_catalog_spec}")
+
+    data_catalogs = {"aws_glue": ("[iceberg_aws]", "glue.GlueCatalog")}
+
+    install_name, module_path = data_catalogs[data_catalog_spec]
+    module, catalog_cls_name = module_path.split(".")
+    try:
+        return getattr(import_module(f"pyiceberg.catalog.{module}"), catalog_cls_name)
+    except ImportError as exc:
+        raise ImportError(
+            f"Package {exc.name} is missing: "
+            f"do 'pip install quixstreams{install_name}' to use "
+            f"data_catalog_spec {data_catalog_spec}"
+        ) from exc
