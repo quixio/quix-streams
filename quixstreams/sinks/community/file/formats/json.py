678664ce446a421ac32db95e199070a232758836
Remy Gwaramadze <remy@quix.io>
2024-10-29 13:35:49 +0100

    JSONFormat docstring

--- quixstreams/sinks/community/file/formats/json.py
+++ quixstreams/sinks/community/file/formats/json.py
@@ -12,13 +12,29 @@ __all__ = ["JSONFormat"]
 
 
 class JSONFormat(BatchFormat):
-    # TODO: Docs
+    """
+    Serializes messages into JSON Lines format with optional gzip compression.
+
+    This class provides functionality to serialize a list of messages into bytes
+    in JSON Lines format. It supports optional gzip compression and allows for
+    custom JSON serialization through the `dumps` parameter.
+    """
+
     def __init__(
         self,
         file_extension: str = ".jsonl",
         compress: bool = False,
         dumps: Optional[Callable[[Any], str]] = None,
     ) -> None:
+        """
+        Initializes the JSONFormat.
+
+        :param file_extension: The file extension to use for output files.
+            Defaults to ".jsonl".
+        :param compress: If `True`, compresses the output using gzip and appends
+            ".gz" to the file extension. Defaults to `False`.
+        :param dumps: A custom function to serialize objects to JSON-formatted strings.
+        """
         self._file_extension = file_extension
 
         self._compress = compress
@@ -36,6 +52,18 @@ class JSONFormat(BatchFormat):
         return self._file_extension
 
     def serialize(self, messages: list[SinkItem]) -> bytes:
+        """
+        Serializes a list of messages into JSON Lines format.
+
+        Each message is converted into a JSON object with "timestamp", "key",
+        and "value" fields. If the message key is in bytes, it is decoded to a
+        string.
+
+        :param messages: The list of messages to serialize.
+        :return: The serialized messages in JSON Lines format, optionally
+            compressed with gzip.
+        """
+
         _to_str = bytes.decode if isinstance(messages[0].key, bytes) else str
 
         with BytesIO() as fp:
