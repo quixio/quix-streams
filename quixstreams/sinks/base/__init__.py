5d999fd44268ae0e2f6179be6cf2fdf11f862802
Remy Gwaramadze <remy@quix.io>
2024-10-29 12:33:53 +0100

    Add SinkItem to quixstreams.sinks.base.__init__

--- quixstreams/sinks/base/__init__.py
+++ quixstreams/sinks/base/__init__.py
@@ -1,12 +1,14 @@
 from .batch import SinkBatch
 from .exceptions import SinkBackpressureError
+from .item import SinkItem
 from .manager import SinkManager
 from .sink import BaseSink, BatchingSink
 
 __all__ = (
-    "SinkBatch",
-    "SinkBackpressureError",
-    "SinkManager",
-    "BatchingSink",
     "BaseSink",
+    "BatchingSink",
+    "SinkBackpressureError",
+    "SinkBatch",
+    "SinkItem",
+    "SinkManager",
 )
