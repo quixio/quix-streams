5dd5d98db883519be0f965d9caea525d495a884b
Tim Sawicki <tim@quix.io>
2024-11-26 15:40:41 -0500

    remove previous blob_store addition

--- quixstreams/sources/community/blob_store/__init__.py
+++
@@ -1,5 +0,0 @@
-# ruff: noqa: F403
-from quixstreams.sources.community.file.formats import *
-
-from .blob_source import *
-from .clients import *
--- quixstreams/sources/community/blob_store/clients/__init__.py
+++
@@ -1,3 +0,0 @@
-# ruff: noqa: F403
-from .aws import *
-from .azure import *
