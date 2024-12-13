89a164b4f7aff3a232499c3c07ebc31385820a7a
Remy Gwaramadze <remy@quix.io>
2024-12-13 12:57:20 +0100

    Rename PREFIX_SEPARATOR to SEPARATOR

--- tests/test_quixstreams/test_state/test_rocksdb/test_windowed/test_serialization.py
+++ tests/test_quixstreams/test_state/test_rocksdb/test_windowed/test_serialization.py
@@ -1,9 +1,9 @@
 import pytest
 
-from quixstreams.state.metadata import PREFIX_SEPARATOR
+from quixstreams.state.metadata import SEPARATOR
 from quixstreams.state.rocksdb.windowed.serialization import (
     append_integer,
-    encode_window_key,
+    encode_integer_pair,
     parse_window_key,
 )
 
@@ -17,8 +17,8 @@ from quixstreams.state.rocksdb.windowed.serialization import (
         (2, -2),
     ],
 )
-def test_encode_window_key(start, end):
-    key = encode_window_key(start_ms=start, end_ms=end)
+def test_encode_integer_pair(start, end):
+    key = encode_integer_pair(start, end)
     assert isinstance(key, bytes)
 
     prefix, decoded_start, decoded_end = parse_window_key(key)
@@ -31,8 +31,6 @@ def test_append_integer(
     base_bytes: bytes,
 ):
     start, end = 0, 10
-    window_key = (
-        base_bytes + PREFIX_SEPARATOR + encode_window_key(start_ms=start, end_ms=end)
-    )
+    window_key = base_bytes + SEPARATOR + encode_integer_pair(start, end)
     window_base_bytes = append_integer(base_bytes=base_bytes, integer=start)
     assert window_key.startswith(window_base_bytes)
