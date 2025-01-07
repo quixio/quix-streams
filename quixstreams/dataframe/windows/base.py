99e67fb9479529901f24d9f7904b54de41a80551
Daniil Gusev <daniil@quix.io>
2025-01-07 16:58:15 +0000

    [WIP] Add a callback to react on late messages in windows

--- quixstreams/dataframe/windows/base.py
+++ quixstreams/dataframe/windows/base.py
@@ -1,5 +1,5 @@
 from collections import deque
-from typing import Any, Callable, Deque, Optional
+from typing import Any, Callable, Deque, Optional, Protocol
 
 from typing_extensions import TypedDict
 
@@ -12,6 +12,34 @@ class WindowResult(TypedDict):
 
 WindowAggregateFunc = Callable[[Any, Any], Any]
 WindowMergeFunc = Callable[[Any], Any]
+
+
+class WindowOnLateCallback(Protocol):
+    def __call__(
+        self,
+        value: Any,
+        timestamp_ms: int,
+        late_by_ms: int,
+        start: int,
+        end: int,
+        store_name: str,
+        topic: str,
+        partition: int,
+        offset: int,
+    ) -> None: ...
+
+
+def default_on_late_callback(
+    value: Any,
+    timestamp_ms: int,
+    late_by_ms: int,
+    start: int,
+    end: int,
+    store_name: str,
+    topic: str,
+    partition: int,
+    offset: int,
+): ...
 
 
 def get_window_ranges(
--- quixstreams/dataframe/windows/definitions.py
+++ quixstreams/dataframe/windows/definitions.py
@@ -2,7 +2,12 @@ import abc
 from abc import abstractmethod
 from typing import TYPE_CHECKING, Any, Callable, Optional, Tuple
 
-from .base import WindowAggregateFunc, WindowMergeFunc
+from .base import (
+    WindowAggregateFunc,
+    WindowMergeFunc,
+    WindowOnLateCallback,
+    default_on_late_callback,
+)
 from .sliding import SlidingWindow
 from .time_based import FixedTimeWindow
 
@@ -23,6 +28,7 @@ class FixedTimeWindowDefinition(abc.ABC):
         dataframe: "StreamingDataFrame",
         name: Optional[str] = None,
         step_ms: Optional[int] = None,
+        on_late: Optional[WindowOnLateCallback] = None,
     ):
         if not isinstance(duration_ms, int):
             raise TypeError("Window size must be an integer")
@@ -41,6 +47,7 @@ class FixedTimeWindowDefinition(abc.ABC):
         self._dataframe = dataframe
         self._name = name
         self._step_ms = step_ms
+        self._on_late = on_late or default_on_late_callback
 
     @abstractmethod
     def _create_window(
@@ -198,6 +205,7 @@ class HoppingWindowDefinition(FixedTimeWindowDefinition):
         step_ms: int,
         dataframe: "StreamingDataFrame",
         name: Optional[str] = None,
+        on_late: Optional[WindowOnLateCallback] = None,
     ):
         super().__init__(
             duration_ms=duration_ms,
@@ -205,6 +213,7 @@ class HoppingWindowDefinition(FixedTimeWindowDefinition):
             dataframe=dataframe,
             name=name,
             step_ms=step_ms,
+            on_late=on_late,
         )
 
     def _get_name(self, func_name: str) -> str:
@@ -227,6 +236,7 @@ class HoppingWindowDefinition(FixedTimeWindowDefinition):
             aggregate_default=aggregate_default,
             merge_func=merge_func,
             dataframe=self._dataframe,
+            on_late=self._on_late,
         )
 
 
@@ -237,9 +247,14 @@ class TumblingWindowDefinition(FixedTimeWindowDefinition):
         grace_ms: int,
         dataframe: "StreamingDataFrame",
         name: Optional[str] = None,
+        on_late: Optional[WindowOnLateCallback] = None,
     ):
         super().__init__(
-            duration_ms=duration_ms, grace_ms=grace_ms, dataframe=dataframe, name=name
+            duration_ms=duration_ms,
+            grace_ms=grace_ms,
+            dataframe=dataframe,
+            name=name,
+            on_late=on_late,
         )
 
     def _get_name(self, func_name: str) -> str:
@@ -261,6 +276,7 @@ class TumblingWindowDefinition(FixedTimeWindowDefinition):
             aggregate_default=aggregate_default,
             merge_func=merge_func,
             dataframe=self._dataframe,
+            on_late=self._on_late,
         )
 
 
@@ -295,4 +311,5 @@ class SlidingWindowDefinition(FixedTimeWindowDefinition):
             aggregate_default=aggregate_default,
             merge_func=merge_func,
             dataframe=self._dataframe,
+            on_late=self._on_late,
         )
