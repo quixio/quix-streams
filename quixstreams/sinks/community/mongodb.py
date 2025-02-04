882a903734a21c29bf2645f9a111cf5bba742230
Tim Sawicki <tim@quix.io>
2025-02-03 20:59:54 -0500

    various cleanup and added value filtering

--- quixstreams/sinks/community/mongodb.py
+++ quixstreams/sinks/community/mongodb.py
@@ -1,9 +1,11 @@
 import datetime
 import logging
 import re
+import sys
+import time
 import uuid
 from functools import partial
-from typing import Callable, Literal, Union
+from typing import Callable, Literal, Optional, Union
 
 import bson
 
@@ -53,20 +55,11 @@ _UPDATE_MAP = {
     "UpdateMany": UpdateMany,
 }
 
-
-def _default_id_setter(record: SinkItem):
-    # TODO: Maybe we just leave the key as-is instead of converting to a string?
-    key = record.key
-    if isinstance(key, bytes):
-        key = key.decode()
-    return str(key)
+MongoQueryFilter = dict[str, MongoValue]
 
 
-def _default_document_matcher(record: SinkItem):
-    key = record.key
-    if isinstance(key, bytes):
-        key = key.decode()
-    return {"_id": str(key)}
+def _default_document_matcher(record: SinkItem) -> MongoQueryFilter:
+    return {"_id": record.key}
 
 
 class MongoDBSink(BatchingSink):
@@ -75,11 +68,14 @@ class MongoDBSink(BatchingSink):
         url: str,
         db: str,
         collection: str,
-        document_matcher: Callable[[SinkItem], MongoValue] = _default_document_matcher,
+        document_matcher: Callable[
+            [SinkItem], MongoQueryFilter
+        ] = _default_document_matcher,
         update_method: Literal["UpdateOne", "UpdateMany", "ReplaceOne"] = "UpdateOne",
         upsert: bool = True,
-        include_message_metadata: bool = False,
-        include_topic_metadata: bool = False,
+        add_message_metadata: bool = False,
+        add_topic_metadata: bool = False,
+        value_selector: Optional[Callable[[MongoValue], MongoValue]] = None,
     ) -> None:
         """
         A connector to sink processed data to MongoDB in batches.
@@ -100,8 +96,14 @@ class MongoDBSink(BatchingSink):
             "UpdateMany": Updates ALL matching documents (usually NOT based on `_id`).
             "ReplaceOne": Replaces the first matching document (usually based on `_id`).
             Default: "UpdateOne".
-        :param include_message_metadata: include key, timestamp, and headers as `__{field}`
-        :param include_topic_metadata: include topic, partition, and offset as `__{field}`
+        :param add_message_metadata: add key, timestamp, and headers as `__{field}`
+        :param add_topic_metadata: add topic, partition, and offset as `__{field}`
+        :param value_selector: An optional callable that allows final editing of the
+            outgoing document state (right before submitting it).
+            This is mostly necessary in special cases, like when a field is required
+            for the `document_matcher` but not desired in the final output.
+            NOTE: metadata is added before this step, so don't accidentally
+            exclude it here!
         """
 
         super().__init__()
@@ -113,22 +115,21 @@ class MongoDBSink(BatchingSink):
         self._document_matcher = document_matcher
         self._update_method = _UPDATE_MAP[update_method]
         self._upsert = upsert
-        self._include_message_metadata = include_message_metadata
-        self._include_topic_metadata = include_topic_metadata
-        # TODO: include a value filter in case additional trimming is desired
-        #  after fields are no longer needed for document_matcher purposes.
+        self._add_message_metadata = add_message_metadata
+        self._add_topic_metadata = add_topic_metadata
+        self._value_selector = value_selector
 
-    def _add_metadata(self, topic: str, partition: int, record: SinkItem):
+    def _add_metadata(self, topic: str, partition: int, record: SinkItem) -> MongoValue:
         value = record.value
-        if self._include_message_metadata:
-            # tuples are pretty much the only thing not covered
-            # TODO: different way to handle this?
+        if self._add_message_metadata:
+            # tuples are one of the few unsupported common types, so let's convert them
+            # so users don't need to manage it.
             value["__key"] = (
                 str(record.key) if isinstance(record.key, tuple) else record.key
             )
             value["__headers"] = dict(record.headers)
             value["__timestamp"] = record.timestamp
-        if self._include_topic_metadata:
+        if self._add_topic_metadata:
             value["__topic"] = topic
             value["__partition"] = partition
             value["__offset"] = record.offset
@@ -140,19 +141,25 @@ class MongoDBSink(BatchingSink):
         network call, and the transaction has size limits...so `bulk_write` is used
         instead, with the downside that duplicate writes may occur if errors arise.
         """
+        _start = time.monotonic()
+        min_timestamp = sys.maxsize
+        max_timestamp = -1
         doc_match = self._document_matcher
         update_method = self._update_method
         upsert = self._upsert
+        value_selector = self._value_selector
         is_update = self._update_method != ReplaceOne
 
-        if self._include_message_metadata or self._include_topic_metadata:
+        if self._add_message_metadata or self._add_topic_metadata:
             added_metadata = partial(self._add_metadata, batch.topic, batch.partition)
         else:
             added_metadata = None
 
         records = []
         for record in batch:
+            ts = record.timestamp
             value = added_metadata(record) if added_metadata else record.value
+            value = value_selector(value) if value_selector else value
             records.append(
                 update_method(
                     doc_match(record) if doc_match else None,
@@ -160,19 +167,25 @@ class MongoDBSink(BatchingSink):
                     upsert=upsert,
                 )
             )
+            min_timestamp = min(ts, min_timestamp)
+            max_timestamp = max(ts, max_timestamp)
         try:
             # We don't need to worry about manually batching our writes, the MongoDB
             # client does it for us automatically (chunks every 10k records).
             # It will block until all writes are successful.
             self._collection.bulk_write(records)
             logger.info(
-                f"MongoDB: successfully submitted {len(records)} update requests to "
-                f'collection "{self._collection_name}" for database "{self._db_name}" '
-                f'at "{self._client.address[0]}"'
+                f"Sent update requests to MongoDB; "
+                f"messages_processed={batch.size} "
+                f"host_address={self._client.address[0]} "
+                f"database={self._db_name} "
+                f"collection={self._collection_name} "
+                f"min_timestamp={min_timestamp} "
+                f"max_timestamp={max_timestamp} "
+                f"time_elapsed={round(time.monotonic() - _start, 2)}s"
             )
         except (NetworkTimeout, ExecutionTimeout, WriteConcernError):
             logger.error("MongoDB: Encountered a server-side error; retrying batch.")
-            # TODO: confirm when SinkBackpressureError should be used?
             raise SinkBackpressureError(
                 retry_after=10, topic=batch.topic, partition=batch.partition
             )
