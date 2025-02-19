0e115e746aa136513a8d3c96ff763efb487ab445
Daniil Gusev <daniil@quix.io>
2025-02-19 13:10:06 +0100

    Fix `QuixPortalApiService` failing in multiprocessing environment (#755)

--- quixstreams/platforms/quix/api.py
+++ quixstreams/platforms/quix/api.py
@@ -13,6 +13,7 @@ from .exceptions import (
 )
 
 __all__ = ("QuixPortalApiService",)
+
 DEFAULT_PORTAL_API_URL = "https://portal-api.platform.quix.io/"
 
 
@@ -37,7 +38,7 @@ class QuixPortalApiService:
         api_version: Optional[str] = None,
         default_workspace_id: Optional[str] = None,
     ):
-        self._portal_api = (
+        self._portal_api_url = (
             portal_api or QUIX_ENVIRONMENT.portal_api or DEFAULT_PORTAL_API_URL
         )
         if not auth_token:
@@ -45,25 +46,14 @@ class QuixPortalApiService:
                 f"A Quix Cloud auth token (SDK or PAT) is required; "
                 f"set with environment variable {QUIX_ENVIRONMENT.SDK_TOKEN}"
             )
-        self._auth_token = auth_token
 
         self._default_workspace_id = (
             default_workspace_id or QUIX_ENVIRONMENT.workspace_id
         )
-        self.api_version = api_version or "2.0"
-        self.session = self._init_session()
-
-    class SessionWithUrlBase(requests.Session):
-        def __init__(self, url_base: str):
-            self.url_base = url_base
-            super().__init__()
-
-        def request(self, method, url, **kwargs):
-            # provided methods have timeout defined, but keep a default for adhoc calls
-            timeout = kwargs.pop("timeout", 30)
-            return super().request(
-                method, urljoin(base=self.url_base, url=url), timeout=timeout, **kwargs
-            )
+        self._request_timeout = 30
+        self.session = self._init_session(
+            api_version=api_version or "2.0", auth_token=auth_token
+        )
 
     @property
     def default_workspace_id(self) -> str:
@@ -78,44 +68,14 @@ class QuixPortalApiService:
     def default_workspace_id(self, value):
         self._default_workspace_id = value
 
-    def _response_handler(self, r: requests.Response, *args, **kwargs):
-        """
-        Custom callback/hook that is called after receiving a request.Response
-
-        Catches non-200's and passes both the original exception and the Response body.
-
-        Note: *args and **kwargs expected for hook
-        """
-        try:
-            r.raise_for_status()
-        except requests.exceptions.HTTPError as e:
-            try:
-                error_text = e.response.json()
-            except requests.exceptions.JSONDecodeError:
-                error_text = e.response.text
-
-            raise QuixApiRequestFailure(
-                status_code=e.response.status_code,
-                url=e.response.url,
-                error_text=error_text,
-            )
-
-    def _init_session(self) -> SessionWithUrlBase:
-        s = self.SessionWithUrlBase(self._portal_api)
-        s.hooks = {"response": self._response_handler}
-        s.headers.update(
-            {
-                "X-Version": self.api_version,
-                "Authorization": f"Bearer {self._auth_token}",
-            }
-        )
-        return s
-
     def get_librdkafka_connection_config(
-        self, workspace_id: Optional[str] = None
+        self, workspace_id: Optional[str] = None, timeout: float = 30
     ) -> dict:
         workspace_id = workspace_id or self.default_workspace_id
-        return self.session.get(f"/workspaces/{workspace_id}/broker/librdkafka").json()
+        return self.session.get(
+            self._build_url(f"/workspaces/{workspace_id}/broker/librdkafka"),
+            timeout=timeout,
+        ).json()
 
     def get_workspace_certificate(
         self, workspace_id: Optional[str] = None, timeout: float = 30
@@ -131,7 +91,7 @@ class QuixPortalApiService:
         """
         workspace_id = workspace_id or self.default_workspace_id
         content = self.session.get(
-            f"/workspaces/{workspace_id}/certificates", timeout=timeout
+            self._build_url(f"/workspaces/{workspace_id}/certificates"), timeout=timeout
         ).content
         if not content:
             return None
@@ -141,24 +101,28 @@ class QuixPortalApiService:
                 return f.read()
 
     def get_auth_token_details(self, timeout: float = 30) -> dict:
-        return self.session.get("/auth/token/details", timeout=timeout).json()
+        return self.session.get(
+            self._build_url("/auth/token/details"), timeout=timeout
+        ).json()
 
     def get_workspace(
         self, workspace_id: Optional[str] = None, timeout: float = 30
     ) -> dict:
         workspace_id = workspace_id or self.default_workspace_id
-        return self.session.get(f"/workspaces/{workspace_id}", timeout=timeout).json()
+        return self.session.get(
+            self._build_url(f"/workspaces/{workspace_id}"), timeout=timeout
+        ).json()
 
     def get_workspaces(self, timeout: float = 30) -> List[dict]:
         # TODO: This seems only return [] with Personal Access Tokens as of Sept 7 '23
-        return self.session.get("/workspaces", timeout=timeout).json()
+        return self.session.get(self._build_url("/workspaces"), timeout=timeout).json()
 
     def get_topic(
         self, topic_name: str, workspace_id: Optional[str] = None, timeout: float = 30
     ) -> dict:
         workspace_id = workspace_id or self.default_workspace_id
         return self.session.get(
-            f"/{workspace_id}/topics/{topic_name}", timeout=timeout
+            self._build_url(f"/{workspace_id}/topics/{topic_name}"), timeout=timeout
         ).json()
 
     def get_topics(
@@ -167,7 +131,9 @@ class QuixPortalApiService:
         timeout: float = 30,
     ) -> List[dict]:
         workspace_id = workspace_id or self.default_workspace_id
-        return self.session.get(f"/{workspace_id}/topics", timeout=timeout).json()
+        return self.session.get(
+            self._build_url(f"/{workspace_id}/topics"), timeout=timeout
+        ).json()
 
     def post_topic(
         self,
@@ -192,5 +158,41 @@ class QuixPortalApiService:
             },
         }
         return self.session.post(
-            f"/{workspace_id}/topics", json=d, timeout=timeout
+            self._build_url(f"/{workspace_id}/topics"), json=d, timeout=timeout
         ).json()
+
+    def _response_handler(self, r: requests.Response, *args, **kwargs):
+        """
+        Custom callback/hook that is called after receiving a request.Response
+
+        Catches non-200's and passes both the original exception and the Response body.
+
+        Note: *args and **kwargs expected for hook
+        """
+        try:
+            r.raise_for_status()
+        except requests.exceptions.HTTPError as e:
+            try:
+                error_text = e.response.json()
+            except requests.exceptions.JSONDecodeError:
+                error_text = e.response.text
+
+            raise QuixApiRequestFailure(
+                status_code=e.response.status_code,
+                url=e.response.url,
+                error_text=error_text,
+            )
+
+    def _build_url(self, path: str) -> str:
+        return urljoin(base=self._portal_api_url, url=path)
+
+    def _init_session(self, api_version: str, auth_token: str) -> requests.Session:
+        session = requests.Session()
+        session.hooks = {"response": self._response_handler}
+        session.headers.update(
+            {
+                "X-Version": api_version,
+                "Authorization": f"Bearer {auth_token}",
+            }
+        )
+        return session
