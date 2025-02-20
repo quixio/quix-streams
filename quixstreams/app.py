d0f444e9c06c5c782db346f38f0aa7568f629aaf
Tim Sawicki <tim@quix.io>
2025-02-20 09:23:31 -0500

    fix after rebase

--- quixstreams/app.py
+++ quixstreams/app.py
@@ -888,6 +888,7 @@ class Application:
         source_manager = self._source_manager
         process_message = self._process_message
         run_tracker = self._run_tracker
+        printer = self._processing_context.printer
 
         processing_context.init_checkpoint()
         run_tracker.start()
@@ -902,6 +903,7 @@ class Application:
                 processing_context.commit_checkpoint()
                 processing_context.resume_ready_partitions()
                 source_manager.raise_for_error()
+                printer.print()
                 run_tracker.update_status()
 
         logger.info("Stop processing of StreamingDataFrame")
