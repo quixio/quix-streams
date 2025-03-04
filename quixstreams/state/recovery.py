9a18c0997e019778906b696d1fdc7d15fa9ec83d
Tim Sawicki <136370015+tim-quix@users.noreply.github.com>
2025-03-04 12:11:51 -0500

    Bugfix for recovering from exactly 1 changelog message (#769)
    * add bugfix and test for off-by-1 error for recovering from exactly 1 changelog message
    
    * remove extra code
    
    * change offset check to is None

--- quixstreams/state/recovery.py
+++ quixstreams/state/recovery.py
@@ -70,8 +70,8 @@ class RecoveryPartition:
 
         :return: changelog offset (int)
         """
-        offset = self._store_partition.get_changelog_offset() or 0
-        if not self._initial_offset:
+        offset = self._store_partition.get_changelog_offset() or -1001
+        if self._initial_offset is None:
             self._initial_offset = offset
         return offset
 
