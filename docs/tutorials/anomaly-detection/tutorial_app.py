d4846b72c744d5328b0ffc729e5482aed7b8f959
Tim Sawicki <136370015+tim-quix@users.noreply.github.com>
2025-03-06 05:36:46 -0500

    update docs and tutorials based on connect callback addition to source and sink (#775)

--- docs/tutorials/anomaly-detection/tutorial_app.py
+++ docs/tutorials/anomaly-detection/tutorial_app.py
@@ -50,6 +50,9 @@ class TemperatureGenerator(Source):
         }
         super().__init__(name="temperature-event-generator")
 
+    def setup(self):
+        return
+
     def update_machine_temp(self, machine_id):
         """
         Updates the temperature for a machine by -1, 0, or 1 based on its current temp.
