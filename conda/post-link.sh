d2a9c77c79031902f75b01dbefacb48d1f9a66e6
Remy Gwaramadze <gwaramadze@users.noreply.github.com>
2025-03-25 18:13:55 +0100

    Upgrade confluent-kafka to >=2.8.2,<2.9 (#799)
    * Upgrade confluent-kafka to >=2.8.2,<2.9
    
    * Fix schema registry tests
    
    * Add schema_registry extra
    
    * missing new line

--- conda/post-link.sh
+++ conda/post-link.sh
@@ -5,4 +5,5 @@ $PREFIX/bin/pip install \
 'protobuf>=5.27.2,<6.0' \
 'influxdb3-python>=0.7,<1.0' \
 'pyiceberg[pyarrow,glue]>=0.7,<0.8' \
-'redis[hiredis]>=5.2.0,<6'
+'redis[hiredis]>=5.2.0,<6' \
+'confluent-kafka[avro,json,protobuf,schemaregistry]>=2.8.2,<2.9'
