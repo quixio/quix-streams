801eca454522e89e41294adaeffa459789e79935
Remy Gwaramadze <gwaramadze@users.noreply.github.com>
2025-04-08 07:32:23 +0000

    Bump protobuf for Conda

--- conda/post-link.sh
+++ conda/post-link.sh
@@ -2,7 +2,7 @@
 
 $PREFIX/bin/pip install \
 'rocksdict>=0.3,<0.4' \
-'protobuf>=5.27.2,<6.0' \
+'protobuf>=5.27.2,<7.0' \
 'influxdb3-python>=0.7,<1.0' \
 'pyiceberg[pyarrow,glue]>=0.7,<0.8' \
 'redis[hiredis]>=5.2.0,<6' \
