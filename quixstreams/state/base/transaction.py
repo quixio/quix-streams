4cb950546c9f112c8f28649dfb4156dc4e8ae8e2
Remy Gwaramadze <remy@quix.io>
2025-04-09 13:57:20 +0200

    Implement TimestampedStore

--- quixstreams/state/base/transaction.py
+++ quixstreams/state/base/transaction.py
@@ -97,6 +97,23 @@ class PartitionTransactionCache:
         # UNDEFINED to signify that
         return self._updated[cf_name][prefix].get(key, Marker.UNDEFINED)
 
+    def iter_items(
+        self, prefix: bytes, backwards: bool = False, cf_name: str = "default"
+    ) -> list[tuple[bytes, bytes]]:
+        """
+        Iterate over sorted, non-deleted items in the cache
+        for the given prefix and column family.
+        """
+        deleted = self._deleted[cf_name]
+        return sorted(
+            (
+                (key, value)
+                for key, value in self._updated[cf_name][prefix].items()
+                if key not in deleted
+            ),
+            reverse=backwards,
+        )
+
     def set(self, key: bytes, value: bytes, prefix: bytes, cf_name: str = "default"):
         """
         Set a value for the key.
