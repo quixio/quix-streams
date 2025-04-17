672f4e8f4748070b831827f3cd7ce8341e1bea13
Quentin Dawans <quentin@quix.io>
2025-04-17 11:25:47 +0200

    dep: remove upper bound for pyiceberg

--- conda/post-link.sh
+++ conda/post-link.sh
@@ -4,6 +4,6 @@ $PREFIX/bin/pip install \
 'rocksdict>=0.3,<0.4' \
 'protobuf>=5.27.2,<7.0' \
 'influxdb3-python>=0.7,<1.0' \
-'pyiceberg[pyarrow,glue]>=0.7,<0.8' \
+'pyiceberg[pyarrow,glue]>=0.7' \
 'redis[hiredis]>=5.2.0,<6' \
 'confluent-kafka[avro,json,protobuf,schemaregistry]>=2.8.2,<2.10'
